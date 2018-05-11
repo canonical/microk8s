b082b2c4464e2dca7a41b7011b4d84dad76eba99
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2018-05-11 10:58:11 +0300

    Attempt to patch docker-default profile

--- build-scripts/create-remove-hook.sh
+++ build-scripts/create-remove-hook.sh
@@ -3,16 +3,14 @@ set -eu
 
 mkdir -p meta/hooks
 
-cat << 'EOF' > meta/hooks/install
+cat << 'EOF' > meta/hooks/remove
 #!/bin/bash
 set -eu
-
-cp -r ${SNAP}/default-args ${SNAP_DATA}/args
 
 #TODO(kjackal): Make sure this works everywhere we want
 if [ -f /etc/apparmor.d/docker ]; then
   echo "Updating docker-default profile"
-  sed 's/^}$/\ \ signal\ (receive)\ peer=snap.microk8s.daemon-docker,\n}/' -i /etc/apparmor.d/docker
+  sed 's/\ \ signal\ (receive)\ peer=snap.microk8s.daemon-docker,\n//' -i /etc/apparmor.d/docker
   echo "Reloading AppArmor profiles"
   service apparmor reload
   echo "AppArmor patched"
@@ -20,4 +18,4 @@ fi
 
 EOF
 
-chmod +x meta/hooks/install
+chmod +x meta/hooks/remove
