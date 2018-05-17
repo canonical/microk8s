29fd15aa4448ad1fd9845ee1c3f266c6f6a9771b
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2018-05-17 20:47:52 +0300

    DNS fix
    You have to:
    - snap install
    - microk8s.enable dns

---
+++ microk8s-resources/actions/disable.dns.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+## remove an option inside the config file.
+skip_opt_in_config() {
+    opt="--$1"
+    config_file="$SNAP_DATA/args/$2"
+    sudo gawk -i inplace '!/^'"$opt"'/ {print $0}' "${config_file}"
+}
+
+# Apply the dns yaml
+# We do not need to see dns pods running at this point just give some slack
+echo "Removing DNS manifest"
+"$SNAP/kubectl" "--kubeconfig=$SNAP/client.config" "delete" "-f" "${SNAP}/actions/dns.yaml"
+sleep 5
+
+echo "Restarting kubelet"
+#TODO(kjackal): do not hardcode the info below. Get it from the yaml
+skip_opt_in_config "cluster-domain" kubelet
+skip_opt_in_config "cluster-dns" kubelet
+
+sudo systemctl restart snap.${SNAP_NAME}.daemon-kubelet
+echo "Done"
