5711c82b5a01da168ea25ca226d74390eb3f00b3
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2018-05-24 15:56:54 +0300

    Fix typo

--- microk8s-resources/actions/disable.storage.sh
+++ microk8s-resources/actions/disable.storage.sh
@@ -10,4 +10,4 @@ sleep 5
 echo "Storage removed"
 read -p "Remove PVC storage at $SNAP_COMMON/default-storage ? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 sudo rm -rf "$SNAP_COMMON/default-storage/*"
-echo "Storage space reclamed"
+echo "Storage space reclaimed"
