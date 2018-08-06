7769308ff86498e053e8b4b4b1312d33100e9ef9
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2018-08-06 11:12:19 +0300

    Use poll instead of blind wait when restarting docker

--- microk8s-resources/actions/enable.gpu.sh
+++ microk8s-resources/actions/enable.gpu.sh
@@ -15,9 +15,20 @@ fi
 
 refresh_opt_in_config "default-runtime" "nvidia" dockerd
 sudo systemctl restart snap.${SNAP_NAME}.daemon-docker
-sleep 10
+TRY_ATTEMPT=0
+while (! (sudo systemctl is-active --quiet snap.${SNAP_NAME}.daemon-docker) ||
+      ! (sudo "$SNAP/usr/bin/docker" "-H" "unix://${SNAP_DATA}/docker.sock" ps &> /dev/null)) &&
+      ! [ ${TRY_ATTEMPT} -eq 6 ]
+do
+  TRY_ATTEMPT=$((TRY_ATTEMPT+1))
+  sleep 5
+done
+if [ ${TRY_ATTEMPT} -eq 6 ]
+then
+  echo "Snapped docker not responding after 30 seconds. Proceeding"
+fi
 
-/snap/bin/microk8s.enable dns
+"$SNAP/microk8s-enable.wrapper" dns
 
 echo "Applying manifest"
 "$SNAP/kubectl" "--kubeconfig=$SNAP/client.config" "apply" "-f" "${SNAP}/actions/gpu.yaml"
