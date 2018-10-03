59865b3837f2b0b652122978979a8a02c510612d
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2018-10-03 20:27:39 +0300

    Kube-proxy not touching conntrack hashsize in containers. Use script … (#125)
    * Kube-proxy not touching conntrack hashsize in containers. Use script to trigger pytest.
    
    * Clarify coment

---
+++ tests/patch-kube-proxy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "--conntrack-max-per-core=0" >> /var/snap/microk8s/current/args/kube-proxy
+systemctl restart snap.microk8s.daemon-proxy
--- tests/test-distro.sh
+++ tests/test-distro.sh
@@ -13,27 +13,37 @@ if echo "$*" | grep -q -- 'help'; then
     exit
 fi
 
-set -ue
+function create_machine() {
+  local NAME=$1
+  if ! lxc profile show microk8s
+  then
+    lxc profile copy default microk8s
+  fi
+  cat tests/lxc/microk8s.profile | lxc profile edit microk8s
+
+  lxc launch -p default -p microk8s $DISTRO $NAME
+  trap "lxc delete ${NAME} --force || true" EXIT
+  # Allow for the machine to boot and get an IP
+  sleep 20
+  tar cf - ./tests | lxc exec $NAME -- tar xvf - -C /tmp
+  lxc exec $NAME -- /bin/bash "/tmp/tests/lxc/install-deps/$DISTRO"
+}
+
+set -uex
 
 DISTRO=$1
 NAME=machine-$RANDOM
 FROM_CHANNEL=$2
 TO_CHANNEL=$3
 
-if ! lxc profile show microk8s
-then
-  lxc profile copy default microk8s
-fi
-cat tests/lxc/microk8s.profile | lxc profile edit microk8s
-
-lxc launch -p default -p microk8s $DISTRO $NAME
-trap "lxc delete ${NAME} --force || true" EXIT
-# Allow for the machine to boot and get an IP
-sleep 20
-
-tar cf - ./tests | lxc exec $NAME -- tar xvf - -C /tmp
-lxc exec $NAME -- /bin/bash "/tmp/tests/lxc/install-deps/$DISTRO"
+create_machine $NAME
 lxc exec $NAME -- snap install microk8s --channel=${TO_CHANNEL} --classic
-lxc exec $NAME -- pytest -s /tmp/tests/test-addons.py
+lxc exec $NAME -- /tmp/tests/patch-kube-proxy.sh
+lxc exec $NAME -- script -e -c "pytest -s /tmp/tests/test-addons.py"
 lxc exec $NAME -- microk8s.reset
-lxc exec $NAME -- /bin/bash -c "UPGRADE_MICROK8S_FROM=${FROM_CHANNEL} UPGRADE_MICROK8S_TO=${TO_CHANNEL} pytest -s /tmp/tests/test-upgrade.py"
+lxc delete $NAME --force
+
+NAME=machine-$RANDOM
+create_machine $NAME
+lxc exec $NAME -- script -e -c "UPGRADE_MICROK8S_FROM=${FROM_CHANNEL} UPGRADE_MICROK8S_TO=${TO_CHANNEL} pytest -s /tmp/tests/test-upgrade.py"
+lxc delete $NAME --force
