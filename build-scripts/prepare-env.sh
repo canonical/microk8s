fd34cc6880b03c8a291ab4022b5d87a2c7be3352
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2018-10-04 10:36:56 +0300

    Have 1.11 branch to build from 1.11 k8s

--- build-scripts/prepare-env.sh
+++ build-scripts/prepare-env.sh
@@ -15,7 +15,7 @@ export ETCD_VERSION="${ETCD_VERSION:-v3.3.4}"
 export CNI_VERSION="${CNI_VERSION:-v0.7.1}"
 export ISTIO_VERSION="${ISTIO_VERSION:-v1.0.0}"
 
-export KUBE_TRACK="${KUBE_TRACK:-}"
+export KUBE_TRACK="${KUBE_TRACK:-1.11}"
 export KUBE_SNAP_BINS="${KUBE_SNAP_BINS:-}"
 if [ -z "$KUBE_SNAP_BINS" ]; then
   if [ -z "$KUBE_TRACK" ]; then
