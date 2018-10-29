196960a4a8b947fb113401e02e18151e3bd6e5f9
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2018-10-29 12:29:29 +0200

    Storage addon to use the right image

--- microk8s-resources/actions/disable.storage.sh
+++ microk8s-resources/actions/disable.storage.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+source $SNAP/actions/common/utils.sh
+
 echo "Disabling default storage"
 cat "${SNAP}/actions/storage.yaml" | \
 "$SNAP/bin/sed" 's@\$SNAP_COMMON@'"$SNAP_COMMON"'@g' | \
+"$SNAP/bin/sed" 's@\$ARCH@'"$ARCH"'@g' | \
 "$SNAP/kubectl" "--kubeconfig=$SNAP/client.config" delete -f -
 sleep 5
 echo "Storage removed"
--- microk8s-resources/actions/enable.storage.sh
+++ microk8s-resources/actions/enable.storage.sh
@@ -2,9 +2,13 @@
 
 set -e
 
+source $SNAP/actions/common/utils.sh
+
 echo "Enabling default storage class"
 sudo mkdir -p ${SNAP_COMMON}/default-storage
+ARCH=$(arch)
 cat "${SNAP}/actions/storage.yaml" | \
 "$SNAP/bin/sed" 's@\$SNAP_COMMON@'"$SNAP_COMMON"'@g' | \
+"$SNAP/bin/sed" 's@\$ARCH@'"$ARCH"'@g' | \
 "$SNAP/kubectl" "--kubeconfig=$SNAP/client.config" apply -f -
 echo "Storage will be available soon"
