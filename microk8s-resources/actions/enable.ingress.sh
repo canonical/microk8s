ddaad5b6ca7831fdf67792acdb100788060d5385
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2019-02-06 20:30:23 +0200

    Fix ingress on arm64

--- microk8s-resources/actions/enable.ingress.sh
+++ microk8s-resources/actions/enable.ingress.sh
@@ -8,13 +8,16 @@ echo "Enabling Ingress"
 
 ARCH=$(arch)
 TAG="0.15.0"
+EXTRA_ARGS="- --publish-status-address=127.0.0.1"
 if [ "${ARCH}" = arm64 ]
 then
   TAG="0.11.0"
+  EXTRA_ARGS=""
 fi
 
 declare -A map
 map[\$TAG]="$TAG"
+map[\$EXTRA_ARGS]="$EXTRA_ARGS"
 use_manifest ingress apply "$(declare -p map)"
 
 echo "Ingress is enabled"
