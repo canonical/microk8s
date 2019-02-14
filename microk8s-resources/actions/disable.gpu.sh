43228f9a56d7de2631e3845b7d6ecccb6f5dfff9
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2019-02-14 20:18:29 +0200

    Fix gpu enable/disable

--- microk8s-resources/actions/disable.gpu.sh
+++ microk8s-resources/actions/disable.gpu.sh
@@ -7,8 +7,7 @@ source $SNAP/actions/common/utils.sh
 echo "Disabling NVIDIA GPU support"
 use_manifest gpu delete
 
-
-sudo sh -c "sed 's@\${SNAP}@'"${SNAP}"'@g;s@\${SNAP_DATA}@'"${SNAP_DATA}"'@g;s@\${RUNTIME}@runc@g' $SNAP_DATA/args/containerd-template.toml > $SNAP_DATA/args/containerd.toml"
+sudo rm -rf ${SNAP_DATA}/var/lock/gpu
 
 sudo systemctl restart snap.${SNAP_NAME}.daemon-containerd
 containerd_up=$(wait_for_service containerd)
