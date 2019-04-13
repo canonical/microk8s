05a14bd0a14bd62b9a7f0c2e3205a92dd90343e8
thor <thor@norse.localdomain>
2019-04-13 17:10:55 +0800

    fix disabling of linkerd to avoid errors when there are no pods with the linkerd proxy.

--- microk8s-resources/actions/disable.linkerd.sh
+++ microk8s-resources/actions/disable.linkerd.sh
@@ -6,8 +6,9 @@ source $SNAP/actions/common/utils.sh
 
 echo "Disabling Linkerd"
 echo "Removing linkerd data plane."
-"$SNAP/kubectl" "--kubeconfig=$SNAP_DATA/credentials/client.config" get --all-namespaces daemonset,deploy,job,statefulset -l "linkerd.io/control-plane-ns" -o yaml | "$SNAP_DATA/bin/linkerd" uninject - | "$SNAP/kubectl" "--kubeconfig=$SNAP_DATA/credentials/client.config" apply -f - 
+#This statement will not terminate the script if there is an error.  Error happens when there is no result returned by getting the resources with label -l "linkerd.io/control-plane-ns"
+"$SNAP/kubectl" "--kubeconfig=$SNAP/client.config" get --all-namespaces daemonset,deploy,job,statefulset -l "linkerd.io/control-plane-ns" -o yaml  | "$SNAP_DATA/bin/linkerd" uninject - | "$SNAP/kubectl" "--kubeconfig=$SNAP/client.config" apply -f -  || true
 echo "Removing linkerd control plane"
-"$SNAP_DATA/bin/linkerd" install | "$SNAP/kubectl" "--kubeconfig=$SNAP_DATA/credentials/client.config" delete -f -
+"$SNAP_DATA/bin/linkerd" install | "$SNAP/kubectl" "--kubeconfig=$SNAP/client.config" delete -f -
 echo "Deleting linkerd binary."
 sudo rm -f "$SNAP_DATA/bin/linkerd"
