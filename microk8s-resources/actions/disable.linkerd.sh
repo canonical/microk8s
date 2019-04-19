85fa29c2abe93490250d98f1a79703778ad76bc9
balchua <balchua@yahoo.com>
2019-04-19 10:35:05 +0800

    upgrade to linkerd v 2.3.0

--- microk8s-resources/actions/disable.linkerd.sh
+++ microk8s-resources/actions/disable.linkerd.sh
@@ -9,6 +9,6 @@ echo "Removing linkerd data plane."
 #This statement will not terminate the script if there is an error.  Error happens when there is no result returned by getting the resources with label -l "linkerd.io/control-plane-ns"
 "$SNAP/kubectl" "--kubeconfig=$SNAP/client.config" get --all-namespaces daemonset,deploy,job,statefulset -l "linkerd.io/control-plane-ns" -o yaml  | "$SNAP_DATA/bin/linkerd" uninject - | "$SNAP/kubectl" "--kubeconfig=$SNAP/client.config" apply -f -  || true
 echo "Removing linkerd control plane"
-"$SNAP_DATA/bin/linkerd" install | "$SNAP/kubectl" "--kubeconfig=$SNAP/client.config" delete -f -
+"$SNAP_DATA/bin/linkerd" "--kubeconfig=$SNAP/client.config" install "--ignore-cluster" | "$SNAP/kubectl" "--kubeconfig=$SNAP/client.config" delete -f -
 echo "Deleting linkerd binary."
 sudo rm -f "$SNAP_DATA/bin/linkerd"
--- microk8s-resources/actions/enable.linkerd.sh
+++ microk8s-resources/actions/enable.linkerd.sh
@@ -9,7 +9,7 @@ argz=("${ARGUMENTS[@]/#/--}")
 
 # check if linkerd cli is already in the system.  Download if it doesn't exist.
 if [ ! -f "${SNAP_DATA}/bin/linkerd" ]; then
-  LINKERD_VERSION="${LINKERD_VERSION:-v2.2.1}"
+  LINKERD_VERSION="${LINKERD_VERSION:-v2.3.0}"
   echo "Fetching Linkerd2 version $LINKERD_VERSION."
   sudo mkdir -p "$SNAP_DATA/bin"
   LINKERD_VERSION=$(echo $LINKERD_VERSION | sed 's/v//g')
@@ -21,5 +21,5 @@ fi
 echo "Enabling Linkerd2"
 # pod/servicegraph will start failing without dns
 "$SNAP/microk8s-enable.wrapper" dns
-"$SNAP_DATA/bin/linkerd" install "${argz[@]}" | "$SNAP/kubectl" "--kubeconfig=$SNAP/client.config" apply -f -
+"$SNAP_DATA/bin/linkerd" "--kubeconfig=$SNAP/client.config" install "${argz[@]}" | "$SNAP/kubectl" "--kubeconfig=$SNAP/client.config" apply -f -
 echo "Linkerd is starting"
