9564cb8184841271b656ac95e4dac29c736dc48f
etheleon <picy2k@gmail.com>
2019-05-04 09:57:44 +0800

    Include Reg as suggested in https://github.com/ubuntu/microk8s/issues/382\#issuecomment-488603844

---
+++ microk8s-resources/actions/enable.reg.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+source $SNAP/actions/common/utils.sh
+
+read -ra ARGUMENTS <<< "$1"
+argz=("${ARGUMENTS[@]/#/--}")
+
+# check if linkerd cli is already in the system.  Download if it doesn't exist.
+if [ ! -f "${SNAP_DATA}/bin/reg" ]; then
+  REG_VERSION="${REG_VERSION:-v0.16.0}"
+  echo "Fetching reg version $REG_VERSION."
+  sudo mkdir -p "$SNAP_DATA/bin"
+  sudo "${SNAP}/usr/bin/curl" -L https://github.com/genuinetools/reg/releases/download/${REG_VERSION}/reg-linux-amd64 -o "$SNAP_DATA/bin/reg"
+  sudo chmod uo+x "$SNAP_DATA/bin/reg"
+fi
+
+echo "REG is installed"
+# pod/servicegraph will start failing without dns
+"$SNAP/microk8s-enable.wrapper" dns
+"$SNAP_DATA/bin/reg" "--kubeconfig=$SNAP/client.config" install "${argz[@]}" | "$SNAP/kubectl" "--kubeconfig=$SNAP/client.config" apply -f -
