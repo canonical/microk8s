2f1f55c7ff8c30d9b418bfd7db303e73647b9ea7
etheleon <picy2k@gmail.com>
2019-05-07 01:28:22 +0800

    Include architecture and rm arguments from enable script

--- microk8s-resources/actions/enable.reg.sh
+++ microk8s-resources/actions/enable.reg.sh
@@ -4,15 +4,12 @@ set -e
 
 source $SNAP/actions/common/utils.sh
 
-read -ra ARGUMENTS <<< "$1"
-argz=("${ARGUMENTS[@]/#/--}")
-
 # check if reg cli is already in the system.  Download if it doesn't exist.
 if [ ! -f "${SNAP_DATA}/bin/reg" ]; then
   REG_VERSION="${REG_VERSION:-v0.16.0}"
   echo "Fetching reg version $REG_VERSION."
   sudo mkdir -p "$SNAP_DATA/bin"
-  sudo "${SNAP}/usr/bin/curl" -L https://github.com/genuinetools/reg/releases/download/${REG_VERSION}/reg-linux-amd64 -o "$SNAP_DATA/bin/reg"
+  sudo "${SNAP}/usr/bin/curl" -L https://github.com/genuinetools/reg/releases/download/${REG_VERSION}/reg-linux-${SNAP_ARCH} -o "$SNAP_DATA/bin/reg"
   sudo chmod uo+x "$SNAP_DATA/bin/reg"
 fi
 
