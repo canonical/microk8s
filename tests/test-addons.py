b833e74d488a86fc6bf2ef0690f8f68fedccaf1a
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2019-06-14 11:19:31 +0300

    Fixes on knative addon enable and tests (#505)
    * Fixes on knative addon enable and tests
    
    * Do not run Istio and Knative tests when under time pressure

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -100,7 +100,7 @@ class TestAddons(object):
         print("Disable gpu")
         microk8s_disable("gpu")
 
-    def test_istio(self):
+    def test_knative_istio(self):
         """
         Sets up and validate istio.
 
@@ -109,30 +109,21 @@ class TestAddons(object):
             print("Istio tests are only relevant in x86 architectures")
             return
 
-        print("Enabling Istio")
-        p = Popen("/snap/bin/microk8s.enable istio".split(), stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        if under_time_pressure != 'False':
+            print("Skipping istio and knative tests as we are under time pressure")
+            return
+
+        print("Enabling Knative and Istio")
+        p = Popen("/snap/bin/microk8s.enable knative".split(), stdout=PIPE, stdin=PIPE, stderr=STDOUT)
         p.communicate(input=b'N\n')[0]
         print("Validating Istio")
         validate_istio()
-        print("Disabling Istio")
-        microk8s_disable("istio")
-
-    def test_knative(self):
-        """
-        Sets up and validate Knative.
-
-        """
-        if platform.machine() != 'x86_64':
-            print("Knative tests are only relevant in x86 architectures (require Istio)")
-            return
-
-        print("Enabling Knative")
-        p = Popen("/snap/bin/microk8s.enable knative".split(), stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-        p.communicate(input=b'N\n')[0]
         print("Validating Knative")
         validate_knative()
         print("Disabling Knative")
         microk8s_disable("knative")
+        print("Disabling Istio")
+        microk8s_disable("istio")
 
     def test_metrics_server(self):
         """
