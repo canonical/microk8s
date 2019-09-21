c9a9731cb1f36416dc883b620d5b6790954e4f87
balchua <balchua@yahoo.com>
2019-09-21 08:11:28 +0800

    fix linkerd tap pod issue with certificate COMMON NAME (CN). Uses CN based on csr.conf

--- microk8s-resources/actions/common/utils.sh
+++ microk8s-resources/actions/common/utils.sh
@@ -274,7 +274,7 @@ gen_server_cert() (
 )
 
 gen_proxy_client_cert() (
-    openssl req -new -key ${SNAP_DATA}/certs/front-proxy-client.key -out ${SNAP_DATA}/certs/front-proxy-client.csr -config ${SNAP_DATA}/certs/csr.conf -subj "/CN=front-proxy-client"
+    openssl req -new -key ${SNAP_DATA}/certs/front-proxy-client.key -out ${SNAP_DATA}/certs/front-proxy-client.csr -config ${SNAP_DATA}/certs/csr.conf
     openssl x509 -req -in ${SNAP_DATA}/certs/front-proxy-client.csr -CA ${SNAP_DATA}/certs/ca.crt -CAkey ${SNAP_DATA}/certs/ca.key -CAcreateserial -out ${SNAP_DATA}/certs/front-proxy-client.crt -days 100000 -extensions v3_ext -extfile ${SNAP_DATA}/certs/csr.conf
 )
 
