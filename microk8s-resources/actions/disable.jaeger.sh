c71d60717cb377ab196eab80d12e5e7168645171
dominik.f <dominik.fleischmann@canonical.com>
2019-10-10 14:34:52 +0200

    Allow jaeger to be deployed in other namespaces
    This allows jaeger to be deployed on other namespaces with the
    following command:
    `microk8s.enable jaeger:<name-of-namespace`
    And to be deleted with this:
    `microk8s.disable jaeger:<name-of-namespace`
    Nonetheless, only one jaeger deployment per microk8s is currently supported

--- microk8s-resources/actions/disable.jaeger.sh
+++ microk8s-resources/actions/disable.jaeger.sh
@@ -3,7 +3,30 @@
 set -e
 source $SNAP/actions/common/utils.sh
 echo "Disabling Jaeger"
+read -ra ARGUMENTS <<< "$1"
+
 KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
-$KUBECTL delete -f "${SNAP}/actions/jaeger"
-$KUBECTL delete -f "${SNAP}/actions/jaeger/crds"
+
+if [ ! -z "${ARGUMENTS[@]}" ]
+then
+
+  $KUBECTL delete -f "${SNAP}/actions/jaeger/crds"
+
+  yaml_path=${SNAP}/actions/jaeger/
+
+  sed "s/namespace: default/namespace: ${ARGUMENTS[0]}/g" $yaml_path/service_account.yaml | $KUBECTL delete -n ${ARGUMENTS[0]} -f -
+  sleep 3
+  sed "s/namespace: default/namespace: ${ARGUMENTS[0]}/g" $yaml_path/role.yaml | $KUBECTL delete -n ${ARGUMENTS[0]} -f -
+  sleep 3
+  sed "s/namespace: default/namespace: ${ARGUMENTS[0]}/g" $yaml_path/role_binding.yaml | $KUBECTL delete -n ${ARGUMENTS[0]} -f -
+  sleep 3
+  KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config -n ${ARGUMENTS[0]}"
+  sed "s/namespace: default/namespace: ${ARGUMENTS[0]}/g" $yaml_path/operator.yaml | $KUBECTL delete -n ${ARGUMENTS[0]} -f -
+
+else
+
+  $KUBECTL delete -f "${SNAP}/actions/jaeger"
+  $KUBECTL delete -f "${SNAP}/actions/jaeger/crds"
+
+fi
 echo "The Jaeger operator is disabled"
--- microk8s-resources/actions/enable.jaeger.sh
+++ microk8s-resources/actions/enable.jaeger.sh
@@ -8,19 +8,44 @@ echo "Enabling Jaeger"
 
 "$SNAP/microk8s-enable.wrapper" dns ingress
 
-KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
-$KUBECTL apply -f "${SNAP}/actions/jaeger/crds"
+read -ra ARGUMENTS <<< "$1"
 
-n=0
-until [ $n -ge 10 ]
-do
+if [ ! -z "${ARGUMENTS[@]}" ]
+then
+
+  KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config -n ${ARGUMENTS[0]}"
+
+  $KUBECTL apply -f "${SNAP}/actions/jaeger/crds"
+
+  yaml_path=${SNAP}/actions/jaeger/
+
+  sed "s/namespace: default/namespace: ${ARGUMENTS[0]}/g" $yaml_path/service_account.yaml | $KUBECTL apply -n ${ARGUMENTS[0]} -f -
   sleep 3
-  ($KUBECTL apply -f "${SNAP}/actions/jaeger/") && break
-  n=$[$n+1]
-  if [ $n -ge 10 ]; then
-    echo "Jaeger operator failed to install"
-    exit 1
-  fi
-done
+  sed "s/namespace: default/namespace: ${ARGUMENTS[0]}/g" $yaml_path/role.yaml | $KUBECTL apply -n ${ARGUMENTS[0]} -f -
+  sleep 3
+  sed "s/namespace: default/namespace: ${ARGUMENTS[0]}/g" $yaml_path/role_binding.yaml | $KUBECTL apply -n ${ARGUMENTS[0]} -f -
+  sleep 3
+  sed "s/namespace: default/namespace: ${ARGUMENTS[0]}/g" $yaml_path/operator.yaml | $KUBECTL apply -n ${ARGUMENTS[0]} -f -
+  sleep 3
+  sed "s/namespace: default/namespace: ${ARGUMENTS[0]}/g" $yaml_path/simplest.yaml | $KUBECTL apply -n ${ARGUMENTS[0]} -f -
+
+else
+
+  KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
+  $KUBECTL apply -f "${SNAP}/actions/jaeger/crds"
+
+  n=0
+  until [ $n -ge 10 ]
+  do
+    sleep 3
+    ($KUBECTL apply -f "${SNAP}/actions/jaeger/") && break
+    n=$[$n+1]
+    if [ $n -ge 10 ]; then
+      echo "Jaeger operator failed to install"
+      exit 1
+    fi
+  done
+
+fi
 
 echo "Jaeger is enabled"
