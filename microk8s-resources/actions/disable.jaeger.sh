fb42f553725893daf959af2aa84ba1a7266dbd42
DomFleischmann <domfleischmann@gmail.com>
2019-10-14 15:35:06 +0200

    Update Jaeger installation files (#711)
    * Update Jaeger installation files
    
    This allows jaeger to be deployed on other namespaces with the
    following command:
    `microk8s.enable jaeger:<name-of-namespace`
    And to be deleted with this:
    `microk8s.disable jaeger:<name-of-namespace`
    Nonetheless, only one jaeger deployment per microk8s is currently supported

--- microk8s-resources/actions/disable.jaeger.sh
+++ microk8s-resources/actions/disable.jaeger.sh
@@ -3,7 +3,27 @@
 set -e
 source $SNAP/actions/common/utils.sh
 echo "Disabling Jaeger"
+read -ra ARGUMENTS <<< "$1"
+
 KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
-$KUBECTL delete -f "${SNAP}/actions/jaeger"
-$KUBECTL delete -f "${SNAP}/actions/jaeger/crds"
+
+if [ ! -z "${ARGUMENTS[@]}" ]
+then
+
+  $KUBECTL delete -f "${SNAP}/actions/jaeger/crds"
+  yaml_path=${SNAP}/actions/jaeger/
+  manifests="service_account.yaml role.yaml role_binding.yaml operator.yaml"
+  KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config -n ${ARGUMENTS[0]}"
+  for yaml in $manifests
+  do
+    sed "s/namespace: default/namespace: ${ARGUMENTS[0]}/g" $yaml_path/$yaml | $KUBECTL delete -f -
+    sleep 3
+  done
+
+else
+
+  $KUBECTL delete -f "${SNAP}/actions/jaeger"
+  $KUBECTL delete -f "${SNAP}/actions/jaeger/crds"
+
+fi
 echo "The Jaeger operator is disabled"
