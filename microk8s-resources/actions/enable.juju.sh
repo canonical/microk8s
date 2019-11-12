dc1e28a53bb3f2eead3bb6c60a423681b4b4ba03
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2019-11-12 13:51:50 +0000

    Get juju not to need sudo

--- microk8s-resources/actions/enable.juju.sh
+++ microk8s-resources/actions/enable.juju.sh
@@ -8,18 +8,27 @@ source $SNAP/actions/common/utils.sh
 function get_juju_client () {
   # check if juju cli is already in the system.  Download if it doesn't exist.
   if [ ! -f "${SNAP_DATA}/bin/juju" ]; then
-    sudo mkdir -p "$SNAP_DATA/bin"
-    sudo mkdir -p "$SNAP_DATA/tmp"
-    sudo snap download juju --channel edge --target-directory="$SNAP_DATA/tmp"
-    sudo unsquashfs -d "$SNAP_DATA/tmp/juju" $SNAP_DATA/tmp/juju_*.snap bin/juju
-    sudo cp "$SNAP_DATA/tmp/juju/bin/juju" "$SNAP_DATA/bin"
+    run_with_sudo mkdir -p "$SNAP_DATA/bin"
+    run_with_sudo mkdir -p "$SNAP_DATA/tmp"
+    run_with_sudo snap download juju --channel edge --target-directory="$SNAP_DATA/tmp"
+    run_with_sudo "$SNAP/usr/bin/unsquashfs" -d "$SNAP_DATA/tmp/juju" $SNAP_DATA/tmp/juju_*.snap bin/juju
+    run_with_sudo cp "$SNAP_DATA/tmp/juju/bin/juju" "$SNAP_DATA/bin"
     # TODO: Re-enable this method when 2.7 hits stable
 #    sudo "${SNAP}/usr/bin/curl" -L https://launchpad.net/juju/2.6/2.6.4/+download/juju-2.6.4-centos7.tar.gz -o "$SNAP_DATA/tmp/juju.tar.gz"
 #    #sudo "${SNAP}/bin/tar" -zxvf "$SNAP_DATA/tmp/juju.tar.gz" -C "$SNAP_DATA/tmp"
 #    sudo tar -zxvf "$SNAP_DATA/tmp/juju.tar.gz" -C "$SNAP_DATA/tmp"
 #    sudo cp "$SNAP_DATA/tmp/juju-bin/juju" "$SNAP_DATA/bin"
-    sudo chmod uo+x "$SNAP_DATA/bin/juju"
-    sudo rm -rf "$SNAP_DATA/tmp/"
+    run_with_sudo chmod uo+x "$SNAP_DATA/bin/juju"
+    run_with_sudo mkdir -p "$SNAP_DATA/juju/share/juju" "$SNAP_DATA/juju-home"
+    run_with_sudo chmod -R ug+rwX "$SNAP_DATA/juju/share/juju" "$SNAP_DATA/juju-home"
+    run_with_sudo chmod -R o-rwX "$SNAP_DATA/juju/share/juju" "$SNAP_DATA/juju-home"
+
+    if getent group microk8s >/dev/null 2>&1
+    then
+      run_with_sudo chgrp microk8s -R "$SNAP_DATA/juju/share/juju" "$SNAP_DATA/juju-home" || true
+    fi
+
+    run_with_sudo rm -rf "$SNAP_DATA/tmp/"
   fi
 }
 
