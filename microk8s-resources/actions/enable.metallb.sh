0a7543580ae118c64a8dc6bcc1502abf9aceb89d
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2019-11-17 20:46:00 +0000

    Making sure the addon works

--- microk8s-resources/actions/enable.metallb.sh
+++ microk8s-resources/actions/enable.metallb.sh
@@ -10,17 +10,17 @@ read -ra ARGUMENTS <<< "$1"
 if [ -z "${ARGUMENTS[@]}" ]
 then
   read -p "Enter the IP address range (e.g., 10.64.140.43-10.64.140.49): " ip_range
-  if [ -z "$var" ]
+  if [ -z "${ip_range}" ]
   then
     echo "You have to input an IP Range value when asked, or provide it as an argument to the enable commang, eg:"
     echo "  microk8s.enable metallb:10.64.140.43-10.64.140.49"
     exit 1
   fi
 else
-  ip_range=ARGUMENTS[0]
+  ip_range="${ARGUMENTS[@]}"
 fi
 
-REGEX_IP_RANGE='^(([(\d+)(x+)]){1,3})(\-+([(\d+)(x)]{1,3}))?\.(([(\d+)(x+)]){1,3})(\-+([(\d+)(x)]{1,3}))?\.(([(\d+)(x+)]){1,3})(\-+([(\d+)(x)]{1,3}))?\.(([(\d+)(x+)]){1,3})(\-+([(\d+)(x)]{1,3}))?$'
+REGEX_IP_RANGE='^[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*-[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$'
 if [[ $ip_range =~ $REGEX_IP_RANGE ]]
 then
   KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
