2a6d2c7c5ec573d6d1c828116c0b58dded785143
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2019-11-17 19:28:42 +0000

    Use iprange from arguments

--- microk8s-resources/actions/enable.metallb.sh
+++ microk8s-resources/actions/enable.metallb.sh
@@ -5,21 +5,29 @@ set -e
 source $SNAP/actions/common/utils.sh
 
 echo "Enabling MetalLB"
-read -p "Enter the IP address range (e.g., 10.64.140.43-10.64.140.49): " ip_range
-REGEX_IP_RANGE='^(([(\d+)(x+)]){1,3})(\-+([(\d+)(x)]{1,3}))?\.(([(\d+)(x+)]){1,3})(\-+([(\d+)(x)]{1,3}))?\.(([(\d+)(x+)]){1,3})(\-+([(\d+)(x)]{1,3}))?\.(([(\d+)(x+)]){1,3})(\-+([(\d+)(x)]{1,3}))?$'
-if [ -z "$var" ]
+
+read -ra ARGUMENTS <<< "$1"
+if [ -z "${ARGUMENTS[@]}" ]
 then
-	echo "You have to input IP Range value"
-        exit 1
+  read -p "Enter the IP address range (e.g., 10.64.140.43-10.64.140.49): " ip_range
+  if [ -z "$var" ]
+  then
+    echo "You have to input an IP Range value when asked, or provide it as an argument to the enable commang, eg:"
+    echo "  microk8s.enable metallb:10.64.140.43-10.64.140.49"
+    exit 1
+  fi
 else
-        if [[ $ip_range =~ $REGEX_IP_RANGE ]]
-	then
-		KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
-		echo "Applying registry manifest"
-		cat ./metallb.yaml | sed "s/{{ip_range}}/$ip_range/g" | $KUBECTL apply -f -
-		echo "MetalLB is enabled"
-	else
-		echo "You input value is not a valid IP Range"
-		exit 1
-	fi
+  ip_range=ARGUMENTS[0]
+fi
+
+REGEX_IP_RANGE='^(([(\d+)(x+)]){1,3})(\-+([(\d+)(x)]{1,3}))?\.(([(\d+)(x+)]){1,3})(\-+([(\d+)(x)]{1,3}))?\.(([(\d+)(x+)]){1,3})(\-+([(\d+)(x)]{1,3}))?\.(([(\d+)(x+)]){1,3})(\-+([(\d+)(x)]{1,3}))?$'
+if [[ $ip_range =~ $REGEX_IP_RANGE ]]
+then
+  KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
+  echo "Applying registry manifest"
+  cat $SNAP/actions/metallb.yaml | $SNAP/bin/sed "s/{{ip_range}}/$ip_range/g" | $KUBECTL apply -f -
+  echo "MetalLB is enabled"
+else
+  echo "You input value ($ip_range) is not a valid IP Range"
+  exit 1
 fi
