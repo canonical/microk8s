2e2fb726e1682c2f25bd0bc30edd441f48838430
Robert Lankford <rlankfo@gmail.com>
2019-11-28 12:52:24 -0600

    pull updated kube-prometheus manifests (#823)
    * pull updated kube-prometheus manifests
    
    * Clean up the old setup

--- microk8s-resources/actions/disable.prometheus.sh
+++ microk8s-resources/actions/disable.prometheus.sh
@@ -8,6 +8,6 @@ echo "Disabling Prometheus"
 KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
 
 $KUBECTL delete -f "${SNAP}/actions/prometheus"
-$KUBECTL delete -f "${SNAP}/actions/prometheus/resources"
+$KUBECTL delete -f "${SNAP}/actions/prometheus/setup"
 
 echo "The Prometheus operator is disabled"
