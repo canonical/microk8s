1ffbf567316067ce6cf009bc9d0511ac2625da16
Kenneth Koski <kenneth.koski@canonical.com>
2020-01-15 09:24:59 -0600

    Add Juju disable script
    Adds script for disabling Juju, which removes the binary, allowing a
    subsequent enabling of Juju to use a different Juju version. Also
    updates Kubeflow disable script to disable Juju as well.

--- microk8s-resources/actions/enable.juju.sh
+++ microk8s-resources/actions/enable.juju.sh
@@ -5,16 +5,21 @@ set -eu
 source $SNAP/actions/common/utils.sh
 
 function get_juju_client () {
-  # check if juju cli is already in the system.  Download if it doesn't exist.
-  if [ ! -f "${SNAP_DATA}/bin/juju" ]; then
-    JUJU_VERSION="${JUJU_VERSION:-2.7.0}"
-    JUJU_SERIES=$(echo $JUJU_VERSION | sed 's|\.[0-9]\+$||')
+  JUJU_VERSION="${JUJU_VERSION:-2.7.0}"
+  JUJU_SERIES=$(echo $JUJU_VERSION | sed 's|\.[0-9]\+$||')
 
+  echo "Installing Juju..."
+
+  # Check if Juju cli is already in the system, and download it if not.
+  if [ -f "${SNAP_DATA}/bin/juju" ]; then
+    EXISTING_VERSION="$(run_with_sudo "${SNAP_DATA}/bin/juju" version)"
+    echo "Juju ${EXISTING_VERSION} is already present."
+  else
     run_with_sudo mkdir -p "$SNAP_DATA/bin"
     run_with_sudo mkdir -p "$SNAP_DATA/tmp"
     run_with_sudo "${SNAP}/usr/bin/curl" -L https://launchpad.net/juju/$JUJU_SERIES/$JUJU_VERSION/+download/juju-$JUJU_VERSION-centos7.tar.gz -o "$SNAP_DATA/tmp/juju.tar.gz"
     run_with_sudo "${SNAP}/bin/tar" -zxvf "$SNAP_DATA/tmp/juju.tar.gz" -C "$SNAP_DATA/tmp"
-    run_with_sudo tar -zxvf "$SNAP_DATA/tmp/juju.tar.gz" -C "$SNAP_DATA/tmp"
+    run_with_sudo tar -zxf "$SNAP_DATA/tmp/juju.tar.gz" -C "$SNAP_DATA/tmp"
     run_with_sudo cp "$SNAP_DATA/tmp/juju-bin/juju" "$SNAP_DATA/bin"
     run_with_sudo chmod uo+x "$SNAP_DATA/bin/juju"
     run_with_sudo mkdir -p "$SNAP_DATA/juju/share/juju" "$SNAP_DATA/juju-home"
@@ -27,10 +32,10 @@ function get_juju_client () {
     fi
 
     run_with_sudo rm -rf "$SNAP_DATA/tmp/"
+    echo "Juju ${JUJU_VERSION} has been installed."
+    echo "Read more about juju at https://docs.jujucharms.com/"
   fi
 }
 
 
-echo "Installing Juju"
 get_juju_client
-echo "Read more about juju at https://docs.jujucharms.com/"
