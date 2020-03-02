0ebbaedb88fa20bdb4f854c5a8528d26941f5c17
Kenneth Koski <kenneth.koski@canonical.com>
2020-03-02 07:24:14 -0500

    Including juju binary in snap instead of downloading it (#989)
    Downloading it with `microk8s.enable juju` leads to lots of networking
    issues with restrictive firewalls/proxies/etc.

--- microk8s-resources/actions/disable.juju.sh
+++ microk8s-resources/actions/disable.juju.sh
@@ -1,18 +1,3 @@
 #!/usr/bin/env bash
 
-set -eu
-
-source $SNAP/actions/common/utils.sh
-
-function disable_juju() {
-  echo "Removing Juju..."
-  if [ -f "${SNAP_DATA}/bin/juju" ]; then
-    run_with_sudo rm -rf "$SNAP_DATA/bin/juju"
-    run_with_sudo rm -rf "$SNAP_DATA/juju"
-    echo "Juju is now disabled."
-  else
-    echo "Juju has already been disabled."
-  fi
-}
-
-disable_juju
+echo "Juju now comes bundled with MicroK8s; this script has been deprecated."
