8392eb855b3aaf10742164b7b01cf61451b4d900
cyril <ccorbon@ccl-consulting.fr>
2020-03-11 23:59:08 +0100

    feature: add option to customize the size of the container registry

--- microk8s-resources/actions/enable.registry.sh
+++ microk8s-resources/actions/enable.registry.sh
@@ -8,7 +8,23 @@ echo "Enabling the private registry"
 
 "$SNAP/microk8s-enable.wrapper" storage
 
-echo "Applying registry manifest"
-use_manifest registry apply
-
-echo "The registry is enabled"
+read -ra ARGUMENTS <<<"$1"
+if [ -z "${ARGUMENTS[@]}" ]; then
+  echo "Your pvc will be created with the default value of 20Gi. You can customize it with the following command eg: microk8s.enable registry:30Gi"
+  declare -A map
+  map[\$DISKSIZE]="20Gi"
+  use_manifest registry apply "$(declare -p map)"
+else
+  disk_size="${ARGUMENTS[@]}"
+  REGEX_DISK_SIZE='(^[2-9][0-9]{1,}|^[1-9][0-9]{2,})(Gi$)'
+  if [[ $disk_size =~ $REGEX_DISK_SIZE ]]; then
+    echo "Applying registry manifest"
+    declare -A map
+    map[\$DISKSIZE]=$disk_size
+    use_manifest registry apply "$(declare -p map)"
+    echo "The registry is enabled"
+  else
+    echo "You input value ($disk_size) is not a valid value"
+    exit 1
+  fi
+fi
