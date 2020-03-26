839425e125dde8819ff46f925e5489e9e2d7e7f3
Konstantinos <kos.tsakalozos@canonical.com>
2020-03-26 14:32:50 +0200

    Reorg tests to fail fast

--- tests/test-distro.sh
+++ tests/test-distro.sh
@@ -53,8 +53,15 @@ then
   PROXY=$4
 fi
 
+# Test addons upgrade
+# TODO Handle local in the upgrade
+create_machine $NAME $PROXY
+# use 'script' for required tty: https://github.com/lxc/lxd/issues/1724#issuecomment-194416774
+lxc exec $NAME -- script -e -c "UPGRADE_MICROK8S_FROM=${FROM_CHANNEL} UPGRADE_MICROK8S_TO=${TO_CHANNEL} pytest -s /var/tmp/tests/test-upgrade.py"
+lxc delete $NAME --force
 
 # Test addons
+NAME=machine-$RANDOM
 create_machine $NAME $PROXY
 if [ ${TO_CHANNEL} == "local" ]
 then
@@ -67,12 +74,4 @@ lxc exec $NAME -- /var/tmp/tests/patch-kube-proxy.sh
 # use 'script' for required tty: https://github.com/lxc/lxd/issues/1724#issuecomment-194416774
 lxc exec $NAME -- script -e -c "pytest -s /var/tmp/tests/test-addons.py"
 lxc exec $NAME -- microk8s reset
-lxc delete $NAME --force
-
-# Test addons upgrade
-# TODO Handle local in the upgrade
-NAME=machine-$RANDOM
-create_machine $NAME $PROXY
-# use 'script' for required tty: https://github.com/lxc/lxd/issues/1724#issuecomment-194416774
-lxc exec $NAME -- script -e -c "UPGRADE_MICROK8S_FROM=${FROM_CHANNEL} UPGRADE_MICROK8S_TO=${TO_CHANNEL} pytest -s /var/tmp/tests/test-upgrade.py"
 lxc delete $NAME --force
