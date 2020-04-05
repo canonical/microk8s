58f94f56e2a589bd1a39bca6be036dbcf81189b3
Konstantinos <kos.tsakalozos@canonical.com>
2020-04-05 17:15:25 +0300

    Better flow

--- microk8s-resources/actions/enable.registry.sh
+++ microk8s-resources/actions/enable.registry.sh
@@ -4,8 +4,6 @@ set -e
 
 source $SNAP/actions/common/utils.sh
 
-echo "Enabling the private registry"
-
 regex_args='([a-zA-Z]+=[a-zA-Z0-9]+(,|))'
 
 read -ra ARGUMENTS <<<"$1"
@@ -24,23 +22,25 @@ elif [[ ${ARGUMENTS[@]} =~ $regex_args ]]; then
 	for arg in "${args[@]}"; do
 		read -r key value <<<$(echo $arg | gawk -F "=" '{print $1 ,$2}')
     if [ "$key" != "size" ]; then
-      echo "WARNING: The only authorized argument is \"size\", eg  microk8s.enable registry:size=30Gi"
-      echo "WARNING: The \"size\" value should match the regex: ([a-zA-Z]+=[a-zA-Z0-9]+(,|))"
-      echo "WARNING: Ignoring the key: $key and value $value"
+      echo "The only authorized argument is \"size\", eg  microk8s.enable registry:size=30Gi"
+      echo "The \"size\" value should match the regex: ([a-zA-Z]+=[a-zA-Z0-9]+(,|))"
+      exit 1
     elif [ "$key" = "size"  ] && [[ ! $value =~ $REGEX_DISK_SIZE ]]; then
       echo "The size of the registry should be higher or equal to 20Gi and match the regex: (^[2-9][0-9]{1,}|^[1-9][0-9]{2,})(Gi$)"
+      exit 1
 		elif [ "$key" = "size" ] && [[ $value =~ $REGEX_DISK_SIZE ]]; then
       "$SNAP/microk8s-enable.wrapper" storage
-      echo "Applying registry manifest"
       declare -A map
       map[\$DISKSIZE]=$value
-      use_manifest registry apply "$(declare -p map)"
-      echo "The registry is enabled"
-      echo "The size of the persistent volume is $value"          
     fi
   done
+  echo "Enabling the private registry"
+  echo "Applying registry manifest"
+  use_manifest registry apply "$(declare -p map)"
+  echo "The registry is enabled"
+  echo "The size of the persistent volume is $value"
 else
-  echo "The only authorized argument is \"size\" and it work as follow: microk8s.enable registry:size=30Gi"
-  echo "The arguments should match the following regex ([a-zA-Z]+=[a-zA-Z0-9]+(,|))"
+  echo "The only authorized argument is \"size\", eg  microk8s.enable registry:size=30Gi"
+  echo "The \"size\" value should match the regex: ([a-zA-Z]+=[a-zA-Z0-9]+(,|))"
   exit 1
 fi
