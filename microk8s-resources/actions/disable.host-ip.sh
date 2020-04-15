cec54a205fd98c8a912eb3afe0af84869702812b
iskitsas <iskitsas@gmail.com>
2020-04-15 13:48:28 +0300

    Tweak script so the arguments passed are named in the following format host-ip:ip=10.0.1.1

--- microk8s-resources/actions/disable.host-ip.sh
+++ microk8s-resources/actions/disable.host-ip.sh
@@ -3,11 +3,14 @@
 set -e
 source $SNAP/actions/common/utils.sh
 
-echo "Disabling host-ip"
-
 if [ -f "${SNAP_DATA}/var/lock/host-ip-enabled" ]
 then
   IP_ADDRESS=$(<"${SNAP_DATA}/var/lock/host-ip-enabled")
-  run_with_sudo "$SNAP/sbin/ifconfig" lo:17 $IP_ADDRESS down
+  echo "Disabling  host-ip [${IP_ADDRESS}]"
+  run_with_sudo "$SNAP/sbin/ifconfig" lo:1 $IP_ADDRESS down
   run_with_sudo "$SNAP/bin/rm" -f "$SNAP_DATA/var/lock/host-ip-enabled"
+  echo "Host-ip is disabled"
+else
+  echo "The host-ip is not enabled. Nothing to do.."
 fi
+
--- microk8s-resources/actions/enable.host-ip.sh
+++ microk8s-resources/actions/enable.host-ip.sh
@@ -4,22 +4,34 @@ set -e
 
 source $SNAP/actions/common/utils.sh
 
-read -ra IP_ADDRESS <<< "$1"
+DEFAULT_IP_ADRESS="10.0.1.1"
+read -ra ARGUMENTS <<<"$1"
+
+read -r key value <<<$(echo "${ARGUMENTS[@]}" | gawk -F "=" '{print $1 ,$2}')
+read -ra IP_ADDRESS <<< "$value"
+
+KEY_NAME="ip"
+if [ ! -z "$key" ] && [ "$key" != $KEY_NAME ]
+then
+  echo "You should use the the '$KEY_NAME' as key in the argument passed and not '$key'. Eg. microk8s.enable host-ip:$KEY_NAME=$IP_ADDRESS";
+  exit
+fi
 
 if [ -z "$IP_ADDRESS" ]
 then
-  IP_ADDRESS="10.0.7.17"
+  IP_ADDRESS="$DEFAULT_IP_ADRESS"
 else
-  IP_ADDRESS="$1"
   if ! valid_ip "${IP_ADDRESS[*]}";
   then
-    echo "Bad IP[${IP_ADDRESS}]. Please provide a valid IP address. Default is 10.0.7.17";
+    echo "Bad IP[${IP_ADDRESS}]. Please provide a valid IP address. Default is $DEFAULT_IP_ADRESS";
     exit
   fi
 fi
 echo "$IP_ADDRESS" > "${SNAP_DATA}/var/lock/host-ip-enabled"
 echo "Setting ${IP_ADDRESS} as host-ip"
 
-run_with_sudo "$SNAP/sbin/ifconfig" lo:17 "$IP_ADDRESS" up
+run_with_sudo "$SNAP/sbin/ifconfig" lo:1 "$IP_ADDRESS" up
+
+echo "Host-ip is enabled"
 
 
