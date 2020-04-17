2d892b4f70d6f44f621950a24ffa9357c8f466be
Giannis E. Skitsas <iskitsas@gmail.com>
2020-04-17 21:41:05 +0300

    Provide a new addon to enable access to host services from pods (#1109)

--- microk8s-resources/actions/common/utils.sh
+++ microk8s-resources/actions/common/utils.sh
@@ -443,6 +443,31 @@ get_all_addons() {
     echo $actions
 }
 
+
+function valid_ip() {
+# Test an IP address for validity:
+# Usage:
+#      valid_ip IP_ADDRESS
+#      if [[ $? -eq 0 ]]; then echo good; else echo bad; fi
+#   OR
+#      if valid_ip IP_ADDRESS; then echo good; else echo bad; fi
+#
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+
 init_cluster() {
   mkdir -p ${SNAP_DATA}/var/kubernetes/backend
   IP="127.0.0.1"
