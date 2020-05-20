e43672af0798cabdd24462290168ec80d670c955
Joe Borg <joseph.borg@canonical.com>
2020-05-20 10:41:04 -0400

    Initial port of NSIS installer

--- installer/windows/generate-self-signed-cert.ps1
+++
@@ -1,3 +0,0 @@
-$cert = New-SelfSignedCertificate -Subject "CN=test-signing.microk8s.io" -Type CodeSigning  -CertStoreLocation "Cert:\CurrentUser\My"
-$CertPassword = ConvertTo-SecureString -String "Password1234" -Force -AsPlainText
-Export-PfxCertificate -Cert "cert:\CurrentUser\My\$($cert.Thumbprint)" -FilePath "test-signing.pfx" -Password $CertPassword
