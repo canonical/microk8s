80e9835fdeddf31b0a31301cbc474ca102fdfaac
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2020-05-20 11:05:39 +0300

    Update helm to v2.16.7 (#1206)

--- microk8s-resources/actions/enable.helm.sh
+++ microk8s-resources/actions/enable.helm.sh
@@ -10,7 +10,7 @@ echo "Enabling Helm"
 if [ ! -f "${SNAP_DATA}/bin/helm" ]
 then
   SOURCE_URI="https://get.helm.sh"
-  HELM_VERSION="v2.16.0"
+  HELM_VERSION="v2.16.7"
 
   echo "Fetching helm version $HELM_VERSION."
   run_with_sudo mkdir -p "${SNAP_DATA}/tmp/helm"
