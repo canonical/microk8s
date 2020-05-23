d8db7f0955a4f79827cbeab9260500a3d087b9f9
balchua <balchua@yahoo.com>
2020-05-23 08:54:33 +0800

    fix all reviews.

--- microk8s-resources/actions/disable.metrics-server.sh
+++ microk8s-resources/actions/disable.metrics-server.sh
@@ -12,19 +12,5 @@ $KUBECTL delete configmap -n kube-system metrics-server-config || true
 $KUBECTL delete deployment -n kube-system metrics-server-v0.2.1 || true 
 
 use_manifest metrics-server delete 
-# wait for pod to terminate before restarting kubelet
-echo "Waiting for pods to be terminated."
-sleep 10
-
-skip_opt_in_config "authentication-token-webhook" kubelet
-skip_opt_in_config "authorization-mode" kubelet
-
-restart_service kubelet
-kubelet=$(wait_for_service kubelet)
-if [[ $kubelet == fail ]]
-then
-  echo "Kubelet did not start on time. Proceeding."
-fi
-sleep 15
 
 echo "Metrics-Server is disabled"
--- microk8s-resources/actions/enable.metrics-server.sh
+++ microk8s-resources/actions/enable.metrics-server.sh
@@ -6,7 +6,7 @@ source $SNAP/actions/common/utils.sh
 
 echo "Enabling Metrics-Server"
 
-"$SNAP/microk8s-enable.wrapper" rbac
+#"$SNAP/microk8s-enable.wrapper" rbac
 
 apiserver=$(wait_for_service apiserver)
 if [[ $apiserver == fail ]]
@@ -18,7 +18,6 @@ KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
 use_manifest metrics-server apply
 
 refresh_opt_in_config "authentication-token-webhook" "true" kubelet
-refresh_opt_in_config "authorization-mode" "Webhook" kubelet
 run_with_sudo preserve_env snapctl restart "${SNAP_NAME}.daemon-kubelet"
 
 echo "Metrics-Server is enabled"
