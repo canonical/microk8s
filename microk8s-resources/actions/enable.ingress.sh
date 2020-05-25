13117da164d4439997f88f60f3824c89dbeaba2b
marcobellaccini <24960638+marcobellaccini@users.noreply.github.com>
2020-05-25 17:05:28 +0200

    Support for NGINX Ingress Controller default certificate
    Added support for optional NGINX Ingress Controller default certificate.
    See https://kubernetes.github.io/ingress-nginx/user-guide/tls/#default-ssl-certificate

--- microk8s-resources/actions/enable.ingress.sh
+++ microk8s-resources/actions/enable.ingress.sh
@@ -4,15 +4,35 @@ set -e
 
 source $SNAP/actions/common/utils.sh
 
+read -ra ARGUMENTS <<<"$1"
+
+read -r key value <<<$(echo "${ARGUMENTS[@]}" | gawk -F "=" '{print $1 ,$2}')
+read -ra CERT_SECRET <<< "$value"
+
+KEY_NAME="defaultcert"
+
+if [ ! -z "$key" ] && [ "$key" != $KEY_NAME ]
+then
+  echo "You should use the the '$KEY_NAME' as key in the argument passed and not '$key'. Eg. microk8s.enable ingress:$KEY_NAME=namespace/secret_name";
+  exit
+fi
+
 echo "Enabling Ingress"
 
 ARCH=$(arch)
 TAG="0.25.1"
 EXTRA_ARGS="- --publish-status-address=127.0.0.1"
+DEFAULT_CERT="- ' '"
 
+if [ ! -z "$CERT_SECRET" ]
+then
+  DEFAULT_CERT="- --default-ssl-certificate=${CERT_SECRET}"
+  echo "Setting ${CERT_SECRET} as default ingress certificate"
+fi
 
 declare -A map
 map[\$TAG]="$TAG"
+map[\$DEFAULT_CERT]="$DEFAULT_CERT"
 map[\$EXTRA_ARGS]="$EXTRA_ARGS"
 use_manifest ingress apply "$(declare -p map)"
 
