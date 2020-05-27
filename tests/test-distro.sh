c256d7a007e2d5a6620196e778839d4dc9499064
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2020-05-27 11:25:21 +0300

    Fixes for default track tan testing istio (#1238)

--- tests/test-distro.sh
+++ tests/test-distro.sh
@@ -4,11 +4,11 @@ if echo "$*" | grep -q -- 'help'; then
     prog=$(basename -s.wrapper "$0")
     echo "Usage: $prog LXC-IMAGE ORIGINAL-CHANNEL UPGRADE-WITH-CHANNEL [PROXY]"
     echo ""
-    echo "Example: $prog ubuntu:18.04 beta edge"
+    echo "Example: $prog ubuntu:18.04 latest/beta latest/edge"
     echo "Use Ubuntu 18.04 for running our tests."
-    echo "We test that microk8s from edge (UPGRADE-WITH-CHANNEL) runs fine."
-    echo "We test that microk8s from beta (ORIGINAL-CHANNEL) can be upgraded"
-    echo "to the revision that is currently on edge (UPGRADE-WITH-CHANNEL)."
+    echo "We test that microk8s from latest/edge (UPGRADE-WITH-CHANNEL) runs fine."
+    echo "We test that microk8s from latest/beta (ORIGINAL-CHANNEL) can be upgraded"
+    echo "to the revision that is currently on latest/edge (UPGRADE-WITH-CHANNEL)."
     echo
     exit
 fi
