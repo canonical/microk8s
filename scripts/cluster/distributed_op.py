ddee2b787c6d99a4953cb591044fd33e1465d7a2
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2020-06-12 01:01:39 +0300

    Handle distribute an operation across nodes (#1297)

--- scripts/cluster/distributed_op.py
+++ scripts/cluster/distributed_op.py
@@ -6,12 +6,17 @@ import requests
 import urllib3
 import os
 import sys
+import json
+import socket
+
+from common.utils import is_node_running_dqlite
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 CLUSTER_API = "cluster/api/v1.0"
 snapdata_path = os.environ.get('SNAP_DATA')
 snap_path = os.environ.get('SNAP')
 callback_tokens_file = "{}/credentials/callback-tokens.txt".format(snapdata_path)
+callback_token_file = "{}/credentials/callback-token.txt".format(snapdata_path)
 
 
 def do_op(remote_op):
@@ -20,21 +25,28 @@ def do_op(remote_op):
     
     :param remote_op: the operation json string
     """
-    with open(callback_tokens_file, "r+") as fp:
-        for _, line in enumerate(fp):
-            parts = line.split()
-            node_ep = parts[0]
-            host = node_ep.split(":")[0]
-            print("Applying to node {}.".format(host))
-            try:
-                # Make sure this node exists
-                subprocess.check_call(
-                    "{}/microk8s-kubectl.wrapper get no {}".format(snap_path, host).split(),
-                    stdout=subprocess.DEVNULL,
-                    stderr=subprocess.DEVNULL,
-                )
-                token = parts[1]
-                remote_op["callback"] = token
+    if is_node_running_dqlite():
+        try:
+            hostname = socket.gethostname()
+            with open(callback_token_file, "r+") as fp:
+                token = fp.read()
+
+            subprocess.check_output(
+                "{}/microk8s-status.wrapper --wait-ready --timeout=60".format(snap_path).split()
+            )
+            nodes_info = subprocess.check_output(
+                "{}/microk8s-kubectl.wrapper get no -o json".format(snap_path).split()
+            )
+            info = json.loads(nodes_info.decode())
+            for node_info in info["items"]:
+                node = node_info['metadata']['name']
+                # TODO: What if the user has set a hostname override in the kubelet args?
+                if node == hostname:
+                    continue
+                print("Configuring node {}".format(node))
+                # TODO: make port configurable
+                node_ep = "{}:{}".format(node, '25000')
+                remote_op["callback"] = token.rstrip()
                 # TODO: handle ssl verification
                 res = requests.post(
                     "https://{}/{}/configure".format(node_ep, CLUSTER_API),
@@ -43,10 +55,43 @@ def do_op(remote_op):
                 )
                 if res.status_code != 200:
                     print(
-                        "Failed to perform a {} on node {}".format(remote_op["action_str"], node_ep)
+                        "Failed to perform a {} on node {} {}".format(
+                            remote_op["action_str"], node_ep, res.status_code
+                        )
                     )
-            except subprocess.CalledProcessError:
-                print("Node {} not present".format(host))
+        except subprocess.CalledProcessError:
+            print("Could not query for nodes")
+
+    else:
+        with open(callback_tokens_file, "r+") as fp:
+            for _, line in enumerate(fp):
+                parts = line.split()
+                node_ep = parts[0]
+                host = node_ep.split(":")[0]
+                print("Applying to node {}.".format(host))
+                try:
+                    # Make sure this node exists
+                    subprocess.check_call(
+                        "{}/microk8s-kubectl.wrapper get no {}".format(snap_path, host).split(),
+                        stdout=subprocess.DEVNULL,
+                        stderr=subprocess.DEVNULL,
+                    )
+                    token = parts[1]
+                    remote_op["callback"] = token
+                    # TODO: handle ssl verification
+                    res = requests.post(
+                        "https://{}/{}/configure".format(node_ep, CLUSTER_API),
+                        json=remote_op,
+                        verify=False,
+                    )
+                    if res.status_code != 200:
+                        print(
+                            "Failed to perform a {} on node {}".format(
+                                remote_op["action_str"], node_ep
+                            )
+                        )
+                except subprocess.CalledProcessError:
+                    print("Node {} not present".format(host))
 
 
 def restart(service):
@@ -120,7 +165,11 @@ def usage():
 
 
 if __name__ == "__main__":
-    if not os.path.isfile(callback_tokens_file):
+    if is_node_running_dqlite() and not os.path.isfile(callback_token_file):
+        print("No callback token file.")
+        exit(1)
+
+    if not is_node_running_dqlite() and not os.path.isfile(callback_tokens_file):
         print("No callback tokens file.")
         exit(1)
 
