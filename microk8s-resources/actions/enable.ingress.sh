0b22fbb8fb44fdf673698c4703496a5235fc9d3f
balchua <balchua@yahoo.com>
2020-06-15 16:06:10 +0800

    use multi arch ingress images. (#1304)

--- microk8s-resources/actions/enable.ingress.sh
+++ microk8s-resources/actions/enable.ingress.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 source $SNAP/actions/common/utils.sh
 
@@ -22,7 +22,7 @@ fi
 echo "Enabling Ingress"
 
 ARCH=$(arch)
-TAG="0.25.1"
+TAG="0.33.0"
 EXTRA_ARGS="- --publish-status-address=127.0.0.1"
 DEFAULT_CERT="- ' '"
 
