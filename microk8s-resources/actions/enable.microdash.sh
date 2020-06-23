1db29fbdabb329ffb80f4b2a7dcb1eb7042aa9ea
iskitsas <iskitsas@gmail.com>
2020-06-23 12:37:16 +0300

    Support a similar to .htpasswd file to keep credentials of the microdash user to login from browser using basic auth

--- microk8s-resources/actions/enable.microdash.sh
+++ microk8s-resources/actions/enable.microdash.sh
@@ -5,6 +5,7 @@ set -e
 source $SNAP/actions/common/utils.sh
 # the base bath '/var/snap/microk8s/current/' could also be retrieved from $SNAP_DATA
 CALLBACK_TOKEN_FILE="${SNAP_DATA}/credentials/callback-token.txt"
+MICRODASH_BASIC_AUTH_FILE="${SNAP_DATA}/credentials/.microdash_basic_auth"
 
 # If token file already exists and has non zero content, use this, otherwise create a new one.
 if [ -f ${CALLBACK_TOKEN_FILE} ] && [ ! -z $(cat $CALLBACK_TOKEN_FILE | sed -r '/^\s*$/d') ]
@@ -27,9 +28,9 @@ else
   exit
 fi
 
-# try to parse the username:password from arguments, otherwise use defaults: microadmin/Qwerty
+# try to parse the username:password from arguments, otherwise use defaults: microadmin/MicroK8s
 MICRODASH_USER="microadmin"
-MICRODASH_PASS=$(openssl passwd -apr1 Qwerty)
+MICRODASH_PASS=$(openssl passwd -apr1 MicroK8s)
 
 read -ra ARGUMENTS <<<"$1"
 read -r user pass <<<$(echo "${ARGUMENTS[@]}" | gawk -F "=" '{print $1 ,$2}')
@@ -38,6 +39,30 @@ if [ ! -z "$user" ] && [ ! -z "$pass" ]
 then
   read -ra MICRODASH_USER <<< "$user"
   read -ra MICRODASH_PASS <<< $(openssl passwd -apr1 $pass)
+fi
+
+# check if nginx basic auth file exists and use that to extract credentials
+# sample:
+#   user1:$apr1$/woC1jnP$KAh0SsVn5qeSMjTtn0E9Q0
+# Hashed password can be created with this command:
+#   openssl passwd -apr1 your_password
+if [ -f ${MICRODASH_BASIC_AUTH_FILE} ] && [ ! -z $(cat $MICRODASH_BASIC_AUTH_FILE | sed -r '/^\s*$/d') ]
+then
+    CONTENT=$(<${MICRODASH_BASIC_AUTH_FILE})
+    read -r user pass <<<$(echo "${CONTENT[@]}" | gawk -F ":" '{print $1 ,$2}')
+    if [ ! -z "$user" ] && [ ! -z "$pass" ]
+    then
+      read -ra MICRODASH_USER <<< "$user"
+      read -ra MICRODASH_PASS <<< "$pass"
+    else
+      echo -e "\nERROR:\nThe file $MICRODASH_BASIC_AUTH_FILE does not include proper data. File content:\n\n $CONTENT\n"
+      echo -e "The username-password pair should be created in NGINX basic auth format using tools like openssl, apache2-utils or httpd-tools."
+      echo -e "For example, you can create a password using the command: \n\n openssl passwd -apr1 your_password \n"
+      echo -e "and then use it along with a desired username by editing the file and creating content like:\n"
+      DUMMY_PASS=$(openssl passwd -apr1 your_password)
+      echo -e "user1:$DUMMY_PASS \n"
+      exit
+    fi
 fi
 
 KUBECTL="$SNAP/microk8s-kubectl.wrapper"
@@ -58,3 +83,5 @@ $KUBECTL create secret generic cb-token --from-literal=token.txt="${TOKEN}" --fr
 
 use_manifest microdash apply
 echo "MicroDash enabled"
+BROWSER_IP=$($KUBECTL get services -n microdash | grep microdash-service | awk '{print $3}')
+echo "Point your browser at $BROWSER_IP and when prompted enter user '$MICRODASH_USER' and the selected password. If nothing selected, default is this 'product name'"
