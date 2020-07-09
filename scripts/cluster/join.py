4ccac1d6f5ea2b975c85b6810ad15eeb28ef9d13
Konstantinos <kos.tsakalozos@canonical.com>
2020-07-09 13:41:51 +0300

    Review 2

--- scripts/cluster/join.py
+++ scripts/cluster/join.py
@@ -136,7 +136,7 @@ def get_etcd_client_cert(master_ip, master_port, token):
     cer_req_file = "{}/certs/server.remote.csr".format(snapdata_path)
     cmd_cert = (
         "{snap}/usr/bin/openssl req -new -sha256 -key {snapdata}/certs/server.key -out {csr} "
-        "-config {SNAP_DATA}/certs/csr.conf".format(
+        "-config {snapdata}/certs/csr.conf".format(
             snap=snap_path, snapdata=snapdata_path, csr=cer_req_file
         )
     )
