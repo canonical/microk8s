b2b112baa8f02d38ae34b79eba245c7b7256e913
Nick Santos <nicholas.j.santos@gmail.com>
2020-08-04 01:10:14 -0400

    enabling the registry should fail if localhost can resolve to an ipv6 address. Fixes https://github.com/ubuntu/microk8s/issues/196

--- microk8s-resources/actions/enable.registry.sh
+++ microk8s-resources/actions/enable.registry.sh
@@ -4,6 +4,14 @@ set -e
 
 source $SNAP/actions/common/utils.sh
 
+hostv6=$(getent ahostsv6 localhost | head -n1 | cut -d' ' -f1)
+if [ "$hostv6" = "::1" ]; then
+  echo "In-cluster registry does not work with ipv6"
+  echo "Remove the mapping from ::1 to localhost in /etc/hosts and try again"
+  echo "For more details, see https://github.com/ubuntu/microk8s/issues/196"
+  exit 1
+fi
+
 echo "Enabling the private registry"
 
 "$SNAP/microk8s-enable.wrapper" storage
