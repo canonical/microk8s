e4a99522465ebb9bce2ac4e60c82099e8c7cf0df
balchua <balchua@yahoo.com>
2020-08-17 16:32:03 +0800

    Upgrade prometheus operator (#1463)

--- microk8s-resources/actions/disable.prometheus.sh
+++ microk8s-resources/actions/disable.prometheus.sh
@@ -4,10 +4,22 @@ set -e
 
 source $SNAP/actions/common/utils.sh
 
-echo "Disabling Prometheus"
 KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
 
-$KUBECTL delete -f "${SNAP}/actions/prometheus"
-$KUBECTL delete -f "${SNAP}/actions/prometheus/setup"
+disable_old_prometheus() {
+  echo "Disabling old Prometheus"
+  $KUBECTL delete -f "${SNAP}/actions/prometheus" > /dev/null 2>&1 || true
+  $KUBECTL delete -f "${SNAP}/actions/prometheus/setup" > /dev/null 2>&1 || true
+}
+
+disable_kube_prometheus() {
+  echo "Disabling Prometheus"
+  $KUBECTL delete -f "${SNAP_DATA}/kube-prometheus/manifests/"
+  $KUBECTL delete -f "${SNAP_DATA}/kube-prometheus/manifests/setup"
+  run_with_sudo rm -rf "${SNAP_DATA}/kube-prometheus"
+}
+
+disable_old_prometheus
+disable_kube_prometheus
 
 echo "The Prometheus operator is disabled"
