85224722409ee3e3c0ea1de3862ae722860bbca8
apnar <jlukens@botch.com>
2020-08-20 03:08:32 -0400

    configure multus to use symlinked current SNAP_DATA locations (#1502)

--- microk8s-resources/actions/enable.multus.sh
+++ microk8s-resources/actions/enable.multus.sh
@@ -5,6 +5,7 @@ set -e
 source "${SNAP}/actions/common/utils.sh"
 
 KUBECTL="${SNAP}/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
+CUR_DATA=$(dirname "${SNAP_DATA}")/current
 
 echo "Enabling Multus"
 
@@ -16,7 +17,7 @@ else
   "${SNAP}/microk8s-status.wrapper" --wait-ready >/dev/null
 
   echo "Applying manifest for multus daemonset."
-  cat "${SNAP}/actions/multus.yaml" | "${SNAP}/bin/sed" "s#{{SNAP_DATA}}#${SNAP_DATA}#g" | ${KUBECTL} apply -f -
+  cat "${SNAP}/actions/multus.yaml" | "${SNAP}/bin/sed" "s#{{SNAP_DATA}}#${CUR_DATA}#g" | ${KUBECTL} apply -f -
 
   echo -n "Waiting for multus daemonset to start."
   until [ -f "${SNAP_DATA}/opt/cni/bin/multus" ]; do
@@ -35,7 +36,7 @@ echo "Currently installed CNI and IPAM plugins include:"
 echo $(cd "${SNAP_DATA}/opt/cni/bin/"; ls)
 
 echo
-echo "New CNI plugins can be installed in ${SNAP_DATA}/opt/cni/bin/"
+echo "New CNI plugins can be installed in ${CUR_DATA}/opt/cni/bin/"
 
 echo
 echo "For information on configuration please refer to the multus documentation."
