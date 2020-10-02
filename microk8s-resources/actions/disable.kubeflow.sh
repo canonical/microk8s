3078c6755f263222dc76eaea7ebf95fc7e1c29c7
Konstantinos <kos.tsakalozos@canonical.com>
2020-10-02 09:54:50 +0000

    Robust disabling of kubeflow

--- microk8s-resources/actions/disable.kubeflow.sh
+++ microk8s-resources/actions/disable.kubeflow.sh
@@ -6,13 +6,8 @@ source $SNAP/actions/common/utils.sh
 
 function disable_kubeflow() {
   echo "Disabling Kubeflow..."
-  if "$SNAP/microk8s-juju.wrapper" show-controller uk8s >/dev/null 2>&1; then
-    "$SNAP/microk8s-juju.wrapper" destroy-controller -y uk8s --destroy-all-models --destroy-storage
-    "$SNAP/microk8s-disable.wrapper" juju
-    echo "Kubeflow is now disabled."
-  else
-    echo "Kubeflow has already been disabled."
-  fi
+  "$SNAP/microk8s-juju.wrapper" unregister -y uk8s || true
+  "$SNAP/microk8s-kubectl.wrapper" delete ns controller-uk8s kubeflow || true
 }
 
 disable_kubeflow
