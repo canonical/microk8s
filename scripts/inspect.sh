c514ccc18f8d244d472aa124c683ea0671a24bd0
Kenneth Koski <kenneth.koski@canonical.com>
2020-10-21 21:08:02 -0500

    Add Juju debugging to microk8s inspect

--- scripts/inspect.sh
+++ scripts/inspect.sh
@@ -103,6 +103,16 @@ function store_kubernetes_info {
 }
 
 
+function store_juju_info {
+  # Collect some juju details
+  printf -- '  Inspect Juju\n'
+  mkdir -p $INSPECT_DUMP/juju
+  sudo -E /snap/bin/microk8s juju status 2>&1 | sudo tee $INSPECT_DUMP/juju/status > /dev/null
+  sudo -E /snap/bin/microk8s juju debug-log 2>&1 | sudo tee $INSPECT_DUMP/juju/debug.log > /dev/null
+  sudo -E /snap/bin/microk8s kubectl logs -n controller-uk8s --tail 10000 -c api-server controller-0 2>&1 | sudo tee $INSPECT_DUMP/juju/controller.log > /dev/null
+}
+
+
 function suggest_fixes {
   # Propose fixes
   printf '\n'
@@ -285,6 +295,9 @@ store_network
 
 printf -- 'Inspecting kubernetes cluster\n'
 store_kubernetes_info
+
+printf -- 'Inspecting juju\n'
+store_juju_info
 
 suggest_fixes
 
