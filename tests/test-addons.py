0e50ac1daa2a594182143193f0dc745a1bd0432e
Josh Lukens <jlukens@botch.com>
2021-01-06 15:22:29 -0500

    Re-add ARM64 support for multus (reverting #1413)

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -329,9 +329,6 @@ class TestAddons(object):
         microk8s_disable("ambassador")
 
     @pytest.mark.skipif(
-        platform.machine() != 'x86_64', reason="Multus tests are only relevant in x86 architectures"
-    )
-    @pytest.mark.skipif(
         os.environ.get('UNDER_TIME_PRESSURE') == 'True',
         reason="Skipping multus tests as we are under time pressure",
     )
