897414bf7657eceb7e2ac097ee0795bc56bf57cf
Joe Borg <joseph.borg@canonical.com>
2021-01-11 17:24:57 -0500

    Hotfix: remove redundant requirements to fix brew install (#1887)

--- installer/setup.py
+++ installer/setup.py
@@ -10,16 +10,13 @@ setup(
     description='MicroK8s is a small, fast, single-package Kubernetes for developers, IoT and edge',
     packages=['cli', 'common', 'vm_providers', 'vm_providers._multipass', 'vm_providers.repo'],
     install_requires=[
-        'setuptools',
         'click~=7.0',
-        'jsonschema==2.5.1',
         'progressbar33==2.4',
         'requests==2.20.0',
         'requests_unixsocket==0.1.5',
         'simplejson==3.8.2',
         'toml==0.10.0',
         'urllib3==1.24.2',
-        'wheel',
     ],
     platforms='any',
     entry_points={
