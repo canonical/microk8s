5072db66a66ab31717f70200b818cdfa8c7edf4f
Sekru <rusekpawel1@gmail.com>
2021-01-18 20:31:08 +0100

    Configurable custom default backend

--- microk8s-resources/actions/enable.ingress.sh
+++ microk8s-resources/actions/enable.ingress.sh
@@ -4,20 +4,33 @@ set -e
 
 source $SNAP/actions/common/utils.sh
 
-read -ra ARGUMENTS <<<"$1"
+read -ra ARGUMENTS <<< $(addon_arguments "$1")
 
-read -r key value <<<$(echo "${ARGUMENTS[@]}" | gawk -F "=" '{print $1 ,$2}')
-read -ra CERT_SECRET <<< "$value"
+CERTIFICATE_KEY_NAME="default-ssl-certificate"
+BACKEND_KEY_NAME="default-backend-service"
 
-KEY_NAME="default-ssl-certificate"
+for ARG in "${ARGUMENTS[@]}"
+do
+  read -r key value <<<$(echo "$ARG" | gawk -F "=" '{print $1 ,$2}')
 
-if [ ! -z "$key" ] && [ "$key" != $KEY_NAME ]
-then
-  echo "Unknown argument '$key'."
-  echo "You can use '$KEY_NAME' to load the default TLS certificate from a secret, eg"
-  echo "   microk8s enable ingress:$KEY_NAME=namespace/secret_name"
-  exit 1
-fi
+  if [ ! -z "$key" ]
+  then
+    if [ "$key" != $CERTIFICATE_KEY_NAME ] && [ "$key" != $BACKEND_KEY_NAME ]
+    then
+      echo "Unknown argument '$key'."
+      echo "You can use '$CERTIFICATE_KEY_NAME' to load the default TLS certificate from a secret, eg"
+      echo "You can use '$BACKEND_KEY_NAME' to change the default backend service"
+      echo "microk8s enable ingress:$CERTIFICATE_KEY_NAME=namespace/secret_name;$BACKEND_KEY_NAME=namespace/service_name"
+      exit 1
+    elif [ "$key" == $CERTIFICATE_KEY_NAME ]
+    then
+      read -ra CERT_SECRET <<< "$value"
+    elif [ "$key" == $BACKEND_KEY_NAME ]
+    then
+      read -ra BACKEND_SERVICE <<< "$value"
+    fi
+  fi
+done
 
 echo "Enabling Ingress"
 
@@ -25,6 +38,7 @@ ARCH=$(arch)
 TAG="v0.35.0"
 EXTRA_ARGS="- --publish-status-address=127.0.0.1"
 DEFAULT_CERT="- ' '"
+DEFAULT_BACKEND_SERVICE="- ' '"
 
 if [ ! -z "$CERT_SECRET" ]
 then
@@ -32,9 +46,16 @@ then
   echo "Setting ${CERT_SECRET} as default ingress certificate"
 fi
 
+if [ ! -z "$BACKEND_SERVICE" ]
+then
+  DEFAULT_BACKEND_SERVICE="- --default-backend-service=${BACKEND_SERVICE}"
+  echo "Setting ${BACKEND_SERVICE} as default ingress service"
+fi
+
 declare -A map
 map[\$TAG]="$TAG"
 map[\$DEFAULT_CERT]="$DEFAULT_CERT"
+map[\$DEFAULT_BACKEND_SERVICE]="$BACKEND_SERVICE"
 map[\$EXTRA_ARGS]="$EXTRA_ARGS"
 use_manifest ingress apply "$(declare -p map)"
 
