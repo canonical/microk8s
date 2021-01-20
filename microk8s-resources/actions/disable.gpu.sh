3df66f68d42127bc0b582462ca6958155361aedd
Joe Borg <joseph.borg@canonical.com>
2021-01-20 15:46:09 -0500

    Don't rewrite containerd template; the operator does that

--- microk8s-resources/actions/disable.gpu.sh
+++ microk8s-resources/actions/disable.gpu.sh
@@ -4,21 +4,8 @@ set -e
 
 source $SNAP/actions/common/utils.sh
 
-if ! [ -e "$SNAP_DATA/var/lock/clustered.lock" ]
-then
-  echo "Disabling NVIDIA GPU support"
-  use_manifest gpu delete
-fi
+echo "Disabling NVIDIA GPU support"
 
-run_with_sudo rm -rf ${SNAP_DATA}/var/lock/gpu
-
-run_with_sudo preserve_env snapctl restart "${SNAP_NAME}.daemon-containerd"
-containerd_up=$(wait_for_service containerd)
-if [[ $containerd_up == fail ]]
-then
-  echo "Containerd did not start on time. Proceeding."
-fi
-# Allow for some seconds for containerd processes to start
-sleep 10
+"$SNAP/microk8s-helm3.wrapper" uninstall gpu-operator
 
 echo "GPU support disabled"
--- microk8s-resources/actions/enable.gpu.sh
+++ microk8s-resources/actions/enable.gpu.sh
@@ -12,20 +12,19 @@ echo "Enabling NVIDIA GPU"
 sudo mkdir -p ${SNAP_DATA}/var/lock
 sudo touch ${SNAP_DATA}/var/lock/gpu
 
-if ! [ -e "$SNAP_DATA/var/lock/clustered.lock" ]
-then
-  # Allow for some seconds for containerd processes to start
-  sleep 10
-  "$SNAP/microk8s-enable.wrapper" helm3
-  echo "Installing NVIDIA Operator"
-  "$SNAP/microk8s-helm3.wrapper" repo add nvidia https://nvidia.github.io/gpu-operator
-  "$SNAP/microk8s-helm3.wrapper" repo update
-  "$SNAP/microk8s-helm3.wrapper" install --generate-name nvidia/gpu-operator \
-    --set operator.defaultRuntime=containerd \
-    --set toolkit.version=1.4.1-ubuntu16.04 \
-    --set toolkit.env[0].name=CONTAINERD_CONFIG \
-    --set toolkit.env[0].value=$CONFIG \
-    --set toolkit.env[1].name=CONTAINERD_SOCKET \
-    --set toolkit.env[1].value=$SOCKET
-  echo "NVIDIA is enabled"
-fi
+"$SNAP/microk8s-enable.wrapper" dns
+"$SNAP/microk8s-enable.wrapper" helm3
+
+echo "Installing NVIDIA Operator"
+
+"$SNAP/microk8s-helm3.wrapper" repo add nvidia https://nvidia.github.io/gpu-operator
+"$SNAP/microk8s-helm3.wrapper" repo update
+"$SNAP/microk8s-helm3.wrapper" install gpu-operator nvidia/gpu-operator \
+  --set operator.defaultRuntime=containerd \
+  --set toolkit.version=1.4.1-ubuntu16.04 \
+  --set toolkit.env[0].name=CONTAINERD_CONFIG \
+  --set toolkit.env[0].value=$CONFIG \
+  --set toolkit.env[1].name=CONTAINERD_SOCKET \
+  --set toolkit.env[1].value=$SOCKET
+
+echo "NVIDIA is enabled"
