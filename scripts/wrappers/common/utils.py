820950b1d4bf66461bb96016d8ee77692910603a
Merlijn Sebrechts <merlijn.sebrechts@gmail.com>
2021-02-17 14:12:53 +0100

    fix instructions for joining microk8s group and cleanup build docs (#2021)
    * suggest safe path for reloading group membership
    
    * Suggest changing ownership of ~/.kube when joining microk8s group
    
    * cleanup build docs

--- scripts/wrappers/common/utils.py
+++ scripts/wrappers/common/utils.py
@@ -150,8 +150,11 @@ def exit_if_no_permission():
         )
         print("")
         print("    sudo usermod -a -G microk8s {}".format(user))
+        print("    sudo chown -f -R $USER ~/.kube")
         print("")
-        print("The new group will be available on the user's next login.")
+        print(
+            "After this, reload the user groups either via a reboot or by running 'newgrp microk8s'."
+        )
         exit(1)
 
 
