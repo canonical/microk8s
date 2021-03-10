ce2909baddf311e6ccfa9490fbf143a022b0071e
Kenneth Koski <kenneth.koski@canonical.com>
2021-03-10 11:26:55 -0600

    Improve Kubeflow addon teardown
    Removes cluster-scoped resources during teardown

--- microk8s-resources/actions/disable.kubeflow.sh
+++ microk8s-resources/actions/disable.kubeflow.sh
@@ -16,7 +16,7 @@ def kubeflow():
     click.echo("Unregistering model...")
     try:
         subprocess.run(
-            ['microk8s-juju.wrapper', 'unregister', '-y', 'uk8s'],
+            ["microk8s-juju.wrapper", "unregister", "-y", "uk8s"],
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -29,7 +29,7 @@ def kubeflow():
     click.echo("Destroying namespace...")
     try:
         subprocess.check_call(
-            ['microk8s-kubectl.wrapper', 'delete', 'ns', 'controller-uk8s', 'kubeflow'],
+            ["microk8s-kubectl.wrapper", "delete", "ns", "controller-uk8s", "kubeflow"],
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -37,10 +37,34 @@ def kubeflow():
         )
     except subprocess.CalledProcessError:
         pass
+
+    # Most resources we just want to remove, but a user may want to keep their
+    # Kubeflow profiles around, so skip deleting the profiles.kubeflow.org CRD.
+    resources = [
+        ("mutatingwebhookconfigurations", ""),
+        ("validatingwebhookconfigurations", ""),
+        ("customresourcedefinitions", ",juju-app notin (kubeflow-dashboard)"),
+        ("clusterroles", ""),
+        ("clusterrolebindings", ""),
+    ]
+
+    for resource, selector in resources:
+        click.echo(f"Destroying Kubeflow {resource}...")
+        try:
+            subprocess.check_call(
+                ["microk8s-kubectl.wrapper", "delete", resource, "-ljuju-app" + selector],
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                env=env,
+            )
+        except subprocess.CalledProcessError:
+            pass
+
     click.echo("Destruction complete.")
 
     click.echo("Kubeflow is now disabled.")
 
 
 if __name__ == "__main__":
-    kubeflow(prog_name='microk8s disable kubeflow')
+    kubeflow(prog_name="microk8s disable kubeflow")
