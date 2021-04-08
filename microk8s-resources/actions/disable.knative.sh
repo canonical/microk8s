a932486cde7ac85e7d38bf7b7cdbd15767749e4d
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2021-04-08 09:28:32 +0300

    Update knative to 0.21

--- microk8s-resources/actions/disable.knative.sh
+++ microk8s-resources/actions/disable.knative.sh
@@ -6,8 +6,23 @@ source $SNAP/actions/common/utils.sh
 
 echo "Disabling Knative"
 
-# || true is there to handle race conditions in deleting resources
 KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
-$KUBECTL delete -f "$SNAP/actions/knative/" || true
+
+declare -a yamls=("https://github.com/knative/serving/releases/download/v0.21.0/serving-core.yaml"
+                  "https://github.com/knative/net-istio/releases/download/v0.21.0/net-istio.yaml"
+                  "https://github.com/knative/serving/releases/download/v0.21.0/serving-default-domain.yaml"
+                  "https://github.com/knative/eventing/releases/download/v0.21.0/eventing-core.yaml"
+                  "https://github.com/knative/eventing/releases/download/v0.21.0/in-memory-channel.yaml"
+                  "https://github.com/knative/eventing/releases/download/v0.21.0/mt-channel-broker.yaml"
+                  "https://github.com/knative/serving/releases/download/v0.21.0/serving-crds.yaml"
+                  "https://github.com/knative/eventing/releases/download/v0.21.0/eventing-crds.yaml"
+                 )
+
+# || true is there to handle race conditions in deleting resources
+for yaml in "${yamls[@]}"
+do
+   $KUBECTL delete -f "$yaml" 2>&1 > /dev/null || true
+   sleep 3
+done
 
 echo "Knative is terminating"
