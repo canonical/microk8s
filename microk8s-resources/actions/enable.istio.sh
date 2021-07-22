ba422c98db260daa2c3c94196cda77e423c81214
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2021-07-22 16:30:00 +0300

    Fixes in enable/disable and tests

--- microk8s-resources/actions/enable.istio.sh
+++ microk8s-resources/actions/enable.istio.sh
@@ -10,17 +10,17 @@ echo "Enabling Istio"
 if [ ! -f "${SNAP_DATA}/bin/istioctl" ]
 then
   ISTIO_VERSION="v1.10.3"
+  echo "Fetching istioctl version $ISTIO_VERSION."
   ISTIO_ERSION=$(echo $ISTIO_VERSION | sed 's/v//g')
-  echo "Fetching istio version $ISTIO_VERSION."
   run_with_sudo mkdir -p "${SNAP_DATA}/tmp/istio"
   (cd "${SNAP_DATA}/tmp/istio"
-  run_with_sudo "${SNAP}/usr/bin/curl" --cacert $CA_CERT -L https://istio.io/downloadIstio -o "$SNAP_DATA/tmp/istio/istio-installer.sh"
-  run_with_sudo chmod +x "$SNAP_DATA/tmp/istio/istio-installer.sh"
-  export ISTIO_VERSION
-  run_with_sudo preserve_env "$SNAP_DATA/tmp/istio/istio-installer.sh"
-  )
+  run_with_sudo "${SNAP}/usr/bin/curl" --cacert $CA_CERT -L https://github.com/istio/istio/releases/download/${ISTIO_ERSION}/istio-${ISTIO_ERSION}-linux-amd64.tar.gz -o "$SNAP_DATA/tmp/isti>
+  run_with_sudo gzip -q -d "$SNAP_DATA/tmp/istio/istio.tar.gz"
+  run_with_sudo tar -xvf "$SNAP_DATA/tmp/istio/istio.tar"
+  run_with_sudo chmod 777 "$SNAP_DATA/tmp/istio/istio-${ISTIO_ERSION}")
   run_with_sudo mkdir -p "$SNAP_DATA/bin/"
   run_with_sudo mv "$SNAP_DATA/tmp/istio/istio-${ISTIO_ERSION}/bin/istioctl" "$SNAP_DATA/bin/"
+  run_with_sudo chmod +x "$SNAP_DATA/bin/"
 
   run_with_sudo rm -rf "$SNAP_DATA/tmp/istio"
 fi
