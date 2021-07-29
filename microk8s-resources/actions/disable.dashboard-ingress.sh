67031e16c8948958844d5cc29945df310ab1c17f
John P Lettman <john.lettman@canonical.com>
2021-07-28 21:23:42 -0400

    Add config args to dashboard-ingress addon (#906)

--- microk8s-resources/actions/disable.dashboard-ingress.sh
+++ microk8s-resources/actions/disable.dashboard-ingress.sh
@@ -1,9 +1,37 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python3
 
-set -e
+import os
+import subprocess
 
-source $SNAP/actions/common/utils.sh
+import click
 
-echo "Disabling Ingress for Kubernetes Dashboard"
-use_manifest dashboard-ingress delete
-echo "Ingress for Kubernetes Dashboard is disabled"
+@click.command()
+def dashboard_ingress():
+    click.echo("Disabling Ingress for Kubernetes Dashboard")
+
+    env = os.environ.copy()
+    env["PATH"] += ":%s" % os.environ["SNAP"]  
+
+    resources = [
+        "secret/kubernetes-dashboard-basic-auth",
+        "ingress.networking.k8s.io/kubernetes-dashboard-ingress"
+    ] 
+
+    for resource in resources:
+        click.echo(f"Destroying {resource}...")
+        try:
+            subprocess.check_call(
+                ["microk8s-kubectl.wrapper", "delete", "-n", "kube-system", resource],
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                env=env,
+            )
+        except subprocess.CalledProcessError:
+            pass
+
+    click.echo("Destruction complete.")
+    click.echo("Ingress for Kubernetes Dashboard is disabled")
+
+if __name__ == "__main__":
+    dashboard_ingress(prog_name="microk8s disable dashboard-ingress")
