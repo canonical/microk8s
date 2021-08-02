59de9a49049a0a271864268383357292b4148536
Elias Koromilas <elias.koromilas@gmail.com>
2021-08-03 00:15:50 +0300

    Print messages in the correct order
    Signed-off-by: Elias Koromilas <elias.koromilas@gmail.com>

--- microk8s-resources/actions/enable.inaccel.sh
+++ microk8s-resources/actions/enable.inaccel.sh
@@ -4,9 +4,9 @@ set -e
 
 source $SNAP/actions/common/utils.sh
 
-echo "Enabling InAccel FPGA Operator"
-
 "$SNAP/microk8s-enable.wrapper" dns helm3
+
+echo "Enabling InAccel FPGA Operator"
 
 "$SNAP/microk8s-helm3.wrapper" install inaccel fpga-operator \
   --repo https://setup.inaccel.com/helm \
