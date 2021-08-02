25e027df7635f8a6c0153994b18a7e2cdbd46f9a
Elias Koromilas <elias.koromilas@gmail.com>
2021-08-02 23:13:08 +0300

    Add enable/disable scripts
    Signed-off-by: Elias Koromilas <elias.koromilas@gmail.com>

---
+++ microk8s-resources/actions/enable.inaccel.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+source $SNAP/actions/common/utils.sh
+
+echo "Enabling InAccel FPGA Operator"
+
+"$SNAP/microk8s-enable.wrapper" dns helm3
+
+"$SNAP/microk8s-helm3.wrapper" install inaccel fpga-operator \
+  --repo https://setup.inaccel.com/helm \
+  --set kubelet=$SNAP_COMMON/var/lib/kubelet \
+  $@
+
+echo "InAccel is enabled"
