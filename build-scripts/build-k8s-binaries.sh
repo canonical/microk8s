65e1e1e5befc5fa54f94171b19c7e9f7bc98972f
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2021-08-25 17:04:38 +0300

    Build k8s binaries without CGO

--- build-scripts/build-k8s-binaries.sh
+++ build-scripts/build-k8s-binaries.sh
@@ -35,12 +35,7 @@ git clone --depth 1 https://github.com/kubernetes/kubernetes $GOPATH/src/github.
   make clean
   for app in ${path_apps}
   do
-    if [ "$app" = "cmd/kubelite" ]
-    then
-      make WHAT="${app}" GOFLAGS=-tags=libsqlite3,dqlite CGO_CFLAGS="-I${SNAPCRAFT_STAGE}/usr/include/" CGO_LDFLAGS="-L${SNAPCRAFT_STAGE}/lib" KUBE_CGO_OVERRIDES=kubelite
-    else
-      make WHAT="${app}"
-    fi
+    make WHAT="${app}"
   done
 )
 for app in $apps; do
