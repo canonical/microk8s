b399de39baa505649f1640dd93761381ee3c8095
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2021-08-27 00:15:14 +0300

    Revert "Building k8s-dqlite in the snap (#2530)" (#2541)
    This reverts commit 1290037237531ddce81e60a60a75d55539d5832c.

--- build-scripts/build-k8s-binaries.sh
+++ build-scripts/build-k8s-binaries.sh
@@ -35,7 +35,12 @@ git clone --depth 1 https://github.com/kubernetes/kubernetes $GOPATH/src/github.
   make clean
   for app in ${path_apps}
   do
-    make WHAT="${app}"
+    if [ "$app" = "cmd/kubelite" ]
+    then
+      make WHAT="${app}" GOFLAGS=-tags=libsqlite3,dqlite CGO_CFLAGS="-I${SNAPCRAFT_STAGE}/usr/include/" CGO_LDFLAGS="-L${SNAPCRAFT_STAGE}/lib" KUBE_CGO_OVERRIDES=kubelite
+    else
+      make WHAT="${app}"
+    fi
   done
 )
 for app in $apps; do
