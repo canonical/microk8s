7255ed7ee21bee5da5e2823b5902ca151ba370f3
balasu <balasundarammaster@gmail.com>
2021-10-28 17:06:06 +0000

    updated portainer disable script

--- microk8s-resources/actions/disable.portainer.sh
+++ microk8s-resources/actions/disable.portainer.sh
@@ -6,7 +6,7 @@ source $SNAP/actions/common/utils.sh
 
 NAMESPACE_PTR="portainer"
 
-MANIFEST_PTR="https://raw.githubusercontent.com/portainer/k8s/master/deploy/manifests/portainer/portainer.yaml"
+#MANIFEST_PTR="https://raw.githubusercontent.com/portainer/k8s/master/deploy/manifests/portainer/portainer.yaml"
 
 KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
 
@@ -15,9 +15,10 @@ KUBECTL_DELETE_ARGS="--wait=true --timeout=180s --ignore-not-found=true"
 echo "Disabling Portainer"
 
 # unload the the manifests
-$KUBECTL delete $KUBECTL_DELETE_ARGS -n $NAMESPACE_PTR -f "$MANIFEST_PTR" > /dev/null 2>&1
+#$KUBECTL delete $KUBECTL_DELETE_ARGS -n $NAMESPACE_PTR -f "$MANIFEST_PTR" > /dev/null 2>&1
+$KUBECTL delete $KUBECTL_DELETE_ARGS -n $NAMESPACE_PTR deployment,service,pods --all  > /dev/null 2>&1
 
 # delete the "portainer" namespace
-$KUBECTL delete $KUBECTL_DELETE_ARGS namespace "$NAMESPACE_PTR" > /dev/null 2>&1 || true
+#$KUBECTL delete $KUBECTL_DELETE_ARGS namespace "$NAMESPACE_PTR" > /dev/null 2>&1 || true
 
 echo "Portainer is disabled"
