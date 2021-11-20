84cb535848e811bb106b72ff253136a727e2fb60
balchua <balchua@yahoo.com>
2021-11-20 12:02:27 +0800

    upgrade keda to the latest version

--- microk8s-resources/actions/enable.keda.sh
+++ microk8s-resources/actions/enable.keda.sh
@@ -16,7 +16,7 @@ do_prerequisites() {
 
 
 get_keda () {
-  KEDA_VERSION="v2.1.0"
+  KEDA_VERSION="v2.4.0"
   KEDA_ERSION=$(echo $KEDA_VERSION | sed 's/v//g')
   echo "Fetching keda version $KEDA_ERSION."
   run_with_sudo "${SNAP}/usr/bin/curl" --cacert $CA_CERT -L https://github.com/kedacore/keda/releases/download/${KEDA_VERSION}/keda-${KEDA_ERSION}.yaml -o "$SNAP_DATA/keda/keda.yaml"
