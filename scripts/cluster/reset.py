156758b4e2dfb5372f19f9b95ceca6238f10b5a9
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2021-11-26 14:04:46 +0200

    Fix removal of callback tokens

--- scripts/cluster/reset.py
+++ scripts/cluster/reset.py
@@ -181,8 +181,10 @@ def remove_callback_token(node):
     with open(tmp_file, "w") as backup_fp:
         os.chmod(tmp_file, 0o600)
         with open(callback_tokens_file, "r+") as callback_fp:
+            # Entries are of the format: 'node_hostname:agent_port token'
+            # We need to get the node_hostname part
             for _, line in enumerate(callback_fp):
-                parts = line.split()
+                parts = line.split(':')
                 if parts[0] == node:
                     continue
                 else:
