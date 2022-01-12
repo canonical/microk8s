0dfe7fb6bda24bffa4d719af6f33e58ffba9a7fb
Joe Borg <joseph.borg@canonical.com>
2022-01-12 14:08:06 -0500

    Pass arguments to disable storage

--- microk8s-resources/actions/disable.storage.sh
+++ microk8s-resources/actions/disable.storage.sh
@@ -7,4 +7,4 @@ source $SNAP/actions/common/utils.sh
 echo "DEPRECIATION WARNING: 'storage' is deprecated and will soon be removed. Please use 'hostpath-storage' instead."
 echo ""
 
-"$SNAP/microk8s-disable.wrapper" hostpath-storage
+"$SNAP/microk8s-disable.wrapper" hostpath-storage $1
