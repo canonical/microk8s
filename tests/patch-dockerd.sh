f870d452ba1598694440719e1bf87ff8ea533652
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2022-01-31 11:19:13 +0200

    Remove dead scripts

--- tests/patch-dockerd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "{
-  \"insecure-registries\" : [\"localhost:32000\"]
-}" > /etc/docker/docker.json
-/bin/systemctl restart docker
--- tests/patch-kube-proxy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-echo "--conntrack-max-per-core=0" >> /var/snap/microk8s/current/args/kube-proxy
-systemctl restart snap.microk8s.daemon-proxy
