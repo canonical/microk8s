de0216fe3fd772bf2b5ca1bb9ceb754336bc0880
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2022-02-23 11:12:44 +0200

    Support JSON and YAML format for microk8s add-node command (#2935)
    * Support JSON and YAML format for microk8s add-node command
    
    Co-authored-by: Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>

--- scripts/cluster/add_token.py
+++ scripts/cluster/add_token.py
@@ -1,3 +1,5 @@
+import json
+import yaml
 import os
 import sys
 import time
@@ -97,6 +99,25 @@ def print_pretty(token, check):
         print(f"microk8s join {ip}:{port}/{token}/{check}")
 
 
+def get_output_dict(token, check):
+    _, all_ips, port = get_network_info()
+    info = {
+        "token": f"{token}/{check}",
+        "urls": [f"{ip}:{port}/{token}/{check}" for ip in all_ips],
+    }
+    return info
+
+
+def print_json(token, check):
+    info = get_output_dict(token, check)
+    print(json.dumps(info, indent=2))
+
+
+def print_yaml(token, check):
+    info = get_output_dict(token, check)
+    print(yaml.dump(info, indent=2))
+
+
 def print_short(token, check):
     default_ip, all_ips, port = get_network_info()
 
@@ -131,7 +152,7 @@ if __name__ == "__main__":
         "--format",
         help="Format the output of the token in pretty, short, token, or token-check",
         default="pretty",
-        choices={"pretty", "short", "token", "token-check"},
+        choices={"pretty", "short", "token", "token-check", "json", "yaml"},
     )
 
     # read arguments from the command line
@@ -157,5 +178,9 @@ if __name__ == "__main__":
         print_short(token, check)
     elif args.format == "token-check":
         print(f"{token}/{check}")
+    elif args.format == "json":
+        print_json(token, check)
+    elif args.format == "yaml":
+        print_yaml(token, check)
     else:
         print(token)
