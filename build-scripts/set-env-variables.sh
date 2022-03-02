ffe0cee27d06542ec7169085b84815b0e1733e77
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2022-03-02 11:45:15 +0200

    Work on tests

--- build-scripts/set-env-variables.sh
+++ build-scripts/set-env-variables.sh
@@ -53,7 +53,7 @@ export ADDONS_REPOS="
 core,${CORE_ADDONS_REPO:-https://github.com/canonical/microk8s-core-addons},${CORE_ADDONS_REPO_BRANCH:-main}
 community,${COMMUNITY_ADDONS_REPO:-https://github.com/canonical/microk8s-addons},${COMMUNITY_ADDONS_REPO_BRANCH:-MK-342/split-core-addons}
 "
-export ADDONS_REPOS_ENABLED="core"
+export ADDONS_REPOS_ENABLED="core community"
 
 echo "Building with:"
 echo "KUBE_VERSION=${KUBE_VERSION}"
