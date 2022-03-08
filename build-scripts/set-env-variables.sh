e80b5d7e502fb9bbf4d875231efbce9bfa3da41a
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2022-03-08 12:35:56 +0200

    Strict patch

--- build-scripts/set-env-variables.sh
+++ build-scripts/set-env-variables.sh
@@ -51,7 +51,7 @@ export KUBE_SNAP_ROOT="$(readlink -f .)"
 
 export ADDONS_REPOS="
 core,${CORE_ADDONS_REPO:-https://github.com/canonical/microk8s-core-addons},${CORE_ADDONS_REPO_BRANCH:-main}
-community,${COMMUNITY_ADDONS_REPO:-https://github.com/canonical/microk8s-community-addons},${COMMUNITY_ADDONS_REPO_BRANCH:-main}
+community,${COMMUNITY_ADDONS_REPO:-https://github.com/canonical/microk8s-community-addons},${COMMUNITY_ADDONS_REPO_BRANCH:-strict}
 "
 export ADDONS_REPOS_ENABLED="core"
 
