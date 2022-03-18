60b2090d6d8e6a12610bd332e07f42597c6dc027
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2022-03-18 19:45:45 +0200

    Properly stop containers

--- microk8s-resources/actions/common/utils.sh
+++ microk8s-resources/actions/common/utils.sh
@@ -666,16 +666,9 @@ get_container_shim_pids() {
 }
 
 kill_all_container_shims() {
-  if (is_strict)
-  then
-    snapctl kill snap.microk8s.daemon-kubelite.service --signal=SIGKILL &>/dev/null || true
-    snapctl kill snap.microk8s.daemon-kubelet.service --signal=SIGKILL &>/dev/null || true
-    snapctl kill snap.microk8s.daemon-containerd.service --signal=SIGKILL &>/dev/null || true
-  else
-    run_with_sudo systemctl kill snap.microk8s.daemon-kubelite.service --signal=SIGKILL &>/dev/null || true
-    run_with_sudo systemctl kill snap.microk8s.daemon-kubelet.service --signal=SIGKILL &>/dev/null || true
-    run_with_sudo systemctl kill snap.microk8s.daemon-containerd.service --signal=SIGKILL &>/dev/null || true
-  fi
+  run_with_sudo systemctl kill snap.microk8s.daemon-kubelite.service --signal=SIGKILL &>/dev/null || true
+  run_with_sudo systemctl kill snap.microk8s.daemon-kubelet.service --signal=SIGKILL &>/dev/null || true
+  run_with_sudo systemctl kill snap.microk8s.daemon-containerd.service --signal=SIGKILL &>/dev/null || true
 }
 
 is_first_boot() {
