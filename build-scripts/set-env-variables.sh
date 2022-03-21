4b4e050a4ad79d5b509e0a18b958641dda47cb38
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2022-03-21 21:08:41 +0200

    Build from strict branches

--- build-scripts/set-env-variables.sh
+++ build-scripts/set-env-variables.sh
@@ -51,7 +51,7 @@ export KUBE_SNAP_ROOT="$(readlink -f .)"
 
 export ADDONS_REPOS="
 core,${CORE_ADDONS_REPO:-https://github.com/canonical/microk8s-core-addons},${CORE_ADDONS_REPO_BRANCH:-strict}
-community,${COMMUNITY_ADDONS_REPO:-https://github.com/canonical/microk8s-addons},${COMMUNITY_ADDONS_REPO_BRANCH:-MK-390/split-core-addons}
+community,${COMMUNITY_ADDONS_REPO:-https://github.com/canonical/microk8s-addons},${COMMUNITY_ADDONS_REPO_BRANCH:-strict}
 "
 export ADDONS_REPOS_ENABLED="core"
 
