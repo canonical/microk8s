6a3abb365bae07956edcf62c57b564f06033288d
Ferran Llamas <llamas.arroniz@gmail.com>
2022-04-13 09:50:50 +0200

    Split tests in individual test modules

--- tests/unit/test_disable.py
+++ tests/unit/test_disable.py
@@ -1,12 +1,27 @@
 from click.testing import CliRunner
-from disable import disable as disable_command
+from disable import disable as command
 from unittest.mock import patch
 
 
-@patch("disable.xable")
-def test_disable_command_shows_addon_help_message(xable_mock):
+def test_command_help_arguments():
+    runner = CliRunner()
+    for help_arg in ("-h", "--help"):
+        result = runner.invoke(command, [help_arg])
+        assert result.exit_code == 0
+        assert "Disables one or more MicroK8s addons" in result.output
+
+
+def test_command_errors_if_no_arguments():
+    runner = CliRunner()
+    result = runner.invoke(command, [])
+    assert result.exit_code != 0
+    assert "Error: Missing argument" in result.output
+
+
+@patch("enable.xable")
+def test_command_shows_addon_help_message(xable_mock):
     runner = CliRunner()
     for help_flag in ("-h", "--help"):
-        result = runner.invoke(disable_command, ["dns", "--", help_flag])
+        result = runner.invoke(command, ["dns", "--", help_flag])
         assert result.output.startswith("Addon dns does not yet have a help message.")
         xable_mock.assert_not_called()
