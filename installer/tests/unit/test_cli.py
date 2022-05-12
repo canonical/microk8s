b64a6bcf9234d89a9f8bd47c5e2bee6de64e15b9
Ferran Llamas <llamas.arroniz@gmail.com>
2022-05-12 10:04:45 +0200

    Exit if requested cpus exceed available

--- installer/tests/unit/test_cli.py
+++ installer/tests/unit/test_cli.py
@@ -7,4 +7,6 @@ import pytest
 def test_install_exits_on_cpus_requested_exceed_available_on_host(has_enough_cpus_mock):
     with pytest.raises(SystemExit) as exc:
         install(MagicMock())
+        
     assert exc.value.code == 1
+    has_enough_cpus_mock.assert_called_once()
