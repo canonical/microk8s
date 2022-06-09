649c36455687fab6204cfcac7bbf45ad0b3ffe87
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2022-06-09 13:38:26 +0300

    Implement "microk8s images export-local" command (#3187)

--- scripts/cluster/images.py
+++ scripts/cluster/images.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+import os
+import subprocess
 import sys
+from typing import List
 
 import click
 
 from distributed_op import do_image_import
+
+CTR = "{}/microk8s-ctr.wrapper".format(os.getenv("SNAP"))
 
 images = click.Group()
 
@@ -23,6 +28,33 @@ def import_images(image: str):
             sys.exit(1)
 
     do_image_import(image_data)
+
+
+def get_all_ctr_images():
+    """
+    Return list of all OCI images from containerd.
+    """
+    images = subprocess.check_output([CTR, "image", "ls", "--quiet"]).decode().split("\n")
+
+    # drop the sha256-digest aliases
+    return [tag for tag in images if tag and not tag.startswith("sha256:")]
+
+
+@images.command("export-local", help="Export OCI images from the current MicroK8s node")
+@click.argument("output", default="-")
+@click.argument("images", nargs=-1)
+def export_images(output: str, images: List[str]):
+    if not images:
+        images = get_all_ctr_images()
+
+    for image in images:
+        click.echo("Checking {}".format(image), err=True)
+        try:
+            subprocess.check_call([CTR, "image", "export", "-", image], stdout=subprocess.DEVNULL)
+        except subprocess.CalledProcessError:
+            subprocess.check_call([CTR, "content", "fetch", "--all-platforms"], stdout=sys.stderr)
+
+    subprocess.check_call([CTR, "image", "export", output, *images])
 
 
 if __name__ == "__main__":
