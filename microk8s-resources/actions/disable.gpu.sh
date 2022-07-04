c25a3f03e3d1460fa382180c8cfc0c9f935fafe9
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2022-07-04 10:13:21 +0300

    Update the GPU addon (#3290)
    * Update the GPU addon
    
    * Update nvidia gpu to v1.11.0
    
    * Right naming for addons

--- microk8s-resources/actions/disable.gpu.sh
+++ microk8s-resources/actions/disable.gpu.sh
@@ -1,11 +1,34 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python3
 
-set -e
+import os
+import json
+import pathlib
+import subprocess
 
-source $SNAP/actions/common/utils.sh
+import click
 
-echo "Disabling NVIDIA GPU support"
+SNAP = pathlib.Path(os.getenv("SNAP") or "/snap/microk8s/current")
+HELM = SNAP / "microk8s-helm3.wrapper"
 
-"$SNAP/microk8s-helm3.wrapper" uninstall gpu-operator
 
-echo "GPU support disabled"
+@click.command()
+def main():
+    click.echo("Disabling NVIDIA GPU support")
+    try:
+        stdout = subprocess.check_output([HELM, "ls", "-A", "-o", "json"])
+        charts = json.loads(stdout)
+    except (OSError, json.JSONDecodeError):
+        click.echo("ERROR: Failed to retrieve installed charts", err=True)
+        charts = []
+
+    for chart in charts:
+        name = chart.get("name")
+        if chart.get("name") == "gpu-operator":
+            namespace = chart.get("namespace") or "default"
+            subprocess.run([HELM, "uninstall", name, "-n", namespace])
+
+    click.echo("GPU support disabled")
+
+
+if __name__ == "__main__":
+    main()
