33fbbbc40a7499c1709d0d7679e99eee0e92baa9
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2022-07-18 12:14:14 +0300

    install raft components

--- build-scripts/components/raft/build.sh
+++ build-scripts/components/raft/build.sh
@@ -12,8 +12,7 @@ mkdir -p build
 
 make install DESTDIR="${PWD}/build"
 
-# mkdir -p "${INSTALL}/bin" "${INSTALL}/lib" "${INSTALL}/usr/include"
+mkdir -p "${INSTALL}/lib" "${INSTALL}/usr/include"
 
-# cp "build/usr/local/bin/sqlite3" "${INSTALL}/bin/sqlite3"
-# cp -r "build/usr/local/lib/libsqlite3"*"so"* "${INSTALL}/lib/"
-# cp -r "build/usr/local/include/"* "${INSTALL}/usr/include/"
+cp -r "build/usr/local/lib/libsqlite3"*"so"* "${INSTALL}/lib/"
+cp -r "build/usr/local/include/"* "${INSTALL}/usr/include/"
