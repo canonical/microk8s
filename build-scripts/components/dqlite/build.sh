7539b7e1134926c641b8afa64965e93e39d95806
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2022-07-18 12:18:40 +0300

    add dqlite component

---
+++ build-scripts/components/dqlite/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+INSTALL="${1}"
+
+[ ! -f ./configure ] && [ -f ./autogen.sh ] && env NOCONFIGURE=1 ./autogen.sh
+[ ! -f ./configure ] && [ -f ./bootstrap ] && env NOCONFIGURE=1 ./bootstrap
+[ ! -f ./configure ] && autoreconf --install
+
+./configure
+
+mkdir -p build
+
+make install DESTDIR="${PWD}/build"
+
+mkdir -p "${INSTALL}/lib" "${INSTALL}/usr/include"
+
+cp -r "build/usr/local/lib/libdqlite"*"so"* "${INSTALL}/lib/"
+cp -r "build/usr/local/include/"* "${INSTALL}/usr/include/"
