358e99a8d95c886b6b433583c3c008ce1cd7294d
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2022-07-29 13:50:56 +0300

    update flanneld to v0.15.1 and remove unused backends

--- build-scripts/components/flanneld/build.sh
+++ build-scripts/components/flanneld/build.sh
@@ -3,17 +3,9 @@
 INSTALL="${1}/opt/cni/bin"
 mkdir -p "${INSTALL}"
 
-# TODO(neoaggelos): update to a non-ancient version of flanneld that we can actually build
-FLANNELD_VERSION="v0.11.0"
-ARCH="${ARCH:-`dpkg --print-architecture`}"
-if [ "$ARCH" = "ppc64el" ]; then
-  ARCH="ppc64le"
-elif [ "$ARCH" = "armhf" ]; then
-  ARCH="arm"
-fi
-mkdir -p download
-cd download
-curl -LO https://github.com/coreos/flannel/releases/download/${FLANNELD_VERSION}/flannel-${FLANNELD_VERSION}-linux-${ARCH}.tar.gz
-tar xvzf flannel-*.tar.gz
+VERSION="${2}"
 
-cp flanneld "${INSTALL}/flanneld"
+export CGO_ENABLED=0
+go build -o dist/flanneld -ldflags "-s -w -X github.com/flannel-io/flannel/version.Version=${VERSION} -extldflags -static"
+
+cp dist/flanneld "${INSTALL}/flanneld"
