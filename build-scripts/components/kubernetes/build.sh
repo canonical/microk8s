d8ab599e87a33159099173f6ed342b416267158c
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2022-08-04 12:15:43 +0300

    minor fixes to snap build process (#3371)

--- build-scripts/components/kubernetes/build.sh
+++ build-scripts/components/kubernetes/build.sh
@@ -1,21 +1,19 @@
 #!/bin/bash -x
 
 INSTALL="${1}"
-
-DIR=`realpath $(dirname "${0}")`
-GIT_TAG="$("${DIR}/version.sh")"
+GIT_TAG="${2}"
 
 if [ ! -d "${SNAPCRAFT_PART_BUILD}/eks-distro" ]; then
-  git clone --depth 1 https://github.com/aws/eks-distro $SNAPCRAFT_PART_BUILD/eks-distro
+  git clone --depth 1 https://github.com/aws/eks-distro "${SNAPCRAFT_PART_BUILD}/eks-distro"
 else
-  (cd $SNAPCRAFT_PART_BUILD/eks-distro && git fetch --all && git pull)
+  (cd "${SNAPCRAFT_PART_BUILD}/eks-distro" && git fetch --all && git pull)
 fi
 
-export EKS_TRACK=$(echo "$GIT_TAG" | cut -f1,2 -d'.' | tr -s . - | cut -c2-)
+EKS_TRACK=$(echo "${GIT_TAG}" | cut -f1,2 -d'.' | tr -s . - | cut -c2-)
 for patch in "${SNAPCRAFT_PART_BUILD}"/eks-distro/projects/kubernetes/kubernetes/"${EKS_TRACK}"/patches/*.patch
 do
-  echo "Applying patch $patch"
-  git am < "$patch"
+  echo "Applying patch ${patch}"
+  git am < "${patch}"
 done
 
 for app in kubectl kubelite; do
@@ -23,7 +21,7 @@ for app in kubectl kubelite; do
   then
     make WHAT="cmd/${app}" GOFLAGS=-tags=libsqlite3,dqlite CGO_CFLAGS="-I${SNAPCRAFT_STAGE}/usr/include/" CGO_LDFLAGS="-L${SNAPCRAFT_STAGE}/lib" CGO_LDFLAGS_ALLOW="-Wl,-z,now" KUBE_CGO_OVERRIDES=kubelite
   else
-    make WHAT="cmd/${app}" KUBE_STATIC_OVERRIDES=kubelite
+    make WHAT="cmd/${app}"
   fi
   cp _output/bin/"${app}" "${INSTALL}/${app}"
 done
