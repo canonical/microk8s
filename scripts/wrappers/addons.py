a4d454741f8b4da0d8cda4ccebe2a543238967a2
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2022-08-18 14:40:38 +0300

    Handle brnaches that changed names for local repos (#3394)

--- scripts/wrappers/addons.py
+++ scripts/wrappers/addons.py
@@ -8,7 +8,7 @@ import sys
 import click
 import yaml
 
-from common.utils import get_current_arch, snap_common
+from common.utils import get_current_arch, snap_common, get_group, snap
 
 GIT = os.path.expandvars("$SNAP/git.wrapper")
 
@@ -16,6 +16,34 @@ addons = click.Group()
 
 repository = click.Group("repo")
 addons.add_command(repository)
+
+
+def pull_and_validate(name: str, repo_dir: Path):
+    commit_before_pull = git_current_commit(repo_dir)
+    subprocess.check_call([GIT, "pull"], cwd=repo_dir)
+
+    try:
+        validate_addons_repo(repo_dir)
+    except RepoValidationError as err:
+        click.echo(err.message, err=True)
+        click.echo(f"Rolling back repository {name}")
+        git_rollback(commit_before_pull, repo_dir)
+        sys.exit(1)
+
+
+def clone_and_validate(remote_url: str, repo_dir: Path):
+    if repo_dir.exists():
+        shutil.rmtree(repo_dir)
+    subprocess.check_call([GIT, "clone", remote_url, repo_dir])
+    subprocess.check_call(["chgrp", get_group(), "-R", repo_dir])
+
+    try:
+        validate_addons_repo(repo_dir)
+    except RepoValidationError as err:
+        click.echo(err.message, err=True)
+        click.echo(f"Removing {repo_dir}")
+        shutil.rmtree(repo_dir)
+        sys.exit(1)
 
 
 @repository.command("add", help="Add a MicroK8s addons repository")
@@ -75,15 +103,29 @@ def update(name: str):
         sys.exit(1)
 
     click.echo("Updating repository {}".format(name))
-    subprocess.check_call([GIT, "pull"], cwd=repo_dir)
-
-    if not (repo_dir / "addons.yaml").exists():
-        click.echo(
-            "Error: repository '{}' does not contain an addons.yaml file".format(name), err=True
+    remote_url = (
+        subprocess.check_output(
+            [GIT, "remote", "get-url", "origin"], cwd=repo_dir, stderr=subprocess.DEVNULL
         )
-        click.echo("Remove it with:", err=True)
-        click.echo("    microk8s addons repo remove {}".format(name), err=True)
-        sys.exit(1)
+        .decode()
+        .strip()
+    )
+    if remote_url.startswith(str(snap().parent)):
+        # This is a repository that we have in the snap.
+        # If the branch name we follow has not changed a simple git pull is enough
+        # If the branch name changed we need to git repo add --force
+        followed_branch_name = subprocess.check_output(
+            [GIT, "rev-parse", "--abbrev-ref", "HEAD"], cwd=repo_dir, stderr=subprocess.DEVNULL
+        ).decode()
+        snapped_branch_name = subprocess.check_output(
+            [GIT, "rev-parse", "--abbrev-ref", "HEAD"], cwd=remote_url, stderr=subprocess.DEVNULL
+        ).decode()
+        if followed_branch_name != snapped_branch_name:
+            clone_and_validate(remote_url, repo_dir)
+        else:
+            pull_and_validate(name, repo_dir)
+    else:
+        pull_and_validate(name, repo_dir)
 
 
 @repository.command("list", help="List configured MicroK8s addons repositories")
