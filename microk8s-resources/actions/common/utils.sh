37fc86044a3b489fec60f8fdb20d12ff2fc215a6
Mariyan Dimitrov <mariyan.dimitrov@gmail.com>
2022-08-27 22:40:18 +0300

    fix(cni): Dump Calico WEPs on stop

--- microk8s-resources/actions/common/utils.sh
+++ microk8s-resources/actions/common/utils.sh
@@ -948,3 +948,16 @@ wait_for_default_route() {
     sleep 2
   done
 }
+
+dump_workload_endpoints() {
+	> "${SNAP_DIR}"/.workload_endpoints
+
+	for podnamespace in \
+		$(microk8s kubectl get po -A -o \
+		jsonpath="{range .items[*]}{.metadata.namespace}{'.'}{.metadata.name}{'\n'}{end}")
+	do
+		digest=$(echo -n $podnamespace | sha1sum | cut -b -11)
+		echo "cali${digest}" >> "${SNAP_DIR}"/.workload_endpoints
+	done
+
+}
