e062dae1dd25b108297c8c926cbde2152e976e6a
Mariyan Dimitrov <mariyan.dimitrov@gmail.com>
2022-08-27 23:38:45 +0300

    refactor(utils): Refactor width

--- microk8s-resources/actions/common/utils.sh
+++ microk8s-resources/actions/common/utils.sh
@@ -950,14 +950,14 @@ wait_for_default_route() {
 }
 
 dump_workload_endpoints() {
-	> "${SNAP_DATA}"/.workload_endpoints
+  > "${SNAP_DATA}"/.workload_endpoints
 
-	for podnamespace in \
-		$(microk8s kubectl get po -A -o \
-		jsonpath="{range .items[*]}{.metadata.namespace}{'.'}{.metadata.name}{'\n'}{end}")
-	do
-		digest=$(echo -n $podnamespace | sha1sum | cut -b -11)
-		echo "cali${digest}" >> "${SNAP_DATA}"/.workload_endpoints
-	done
+  for podnamespace in \
+    $(microk8s kubectl get po -A -o \
+    jsonpath="{range .items[*]}{.metadata.namespace}{'.'}{.metadata.name}{'\n'}{end}")
+    do
+      digest=$(echo -n $podnamespace | sha1sum | cut -b -11)
+      echo "cali${digest}" >> "${SNAP_DATA}"/.workload_endpoints
+    done
 
-}
+  }
