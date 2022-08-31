20ee2267917eade6c34b79ccb0801cebee515326
Berkay Tekin Oz <berkay.tekinoz@canonical.com>
2022-08-31 12:36:15 +0300

    Custom images for eksd and auto add eksd addons

---
+++ build-scripts/images/parse.py
@@ -0,0 +1,20 @@
+import yaml
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit(1)
+    with open(sys.argv[1], "r") as stream:
+        try:
+            manifest = yaml.safe_load(stream)
+            with open(f"{sys.argv[2]}/images-list", "w+") as list:
+                for component in manifest["status"]["components"]:
+                    for asset in component["assets"]:
+                        if asset["name"] == "pause-image":
+                            list.write("PAUSE_IMAGE={}\n".format(asset["image"]["uri"]))
+                        elif asset["name"] == "coredns-image":
+                            list.write("COREDNS_IMAGE={}\n".format(asset["image"]["uri"]))
+                        elif asset["name"] == "metrics-server-image":
+                            list.write("METRICS_SERVER_IMAGE={}\n".format(asset["image"]["uri"]))
+        except yaml.YAMLError as exc:
+            print(exc)
