b48b194e5489c2763601cd0c547a425878ffcb78
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2022-09-08 15:10:25 +0300

    Pull the eksd images in a single file (#3426)
    * Pull the eksd images in a single file
    
    * Fix style errors

--- build-scripts/images/parse.py
+++ build-scripts/images/parse.py
@@ -1,4 +1,3 @@
-import subprocess
 import yaml
 import sys
 
@@ -8,16 +7,14 @@ if __name__ == "__main__":
     with open(sys.argv[1], "r") as stream:
         try:
             manifest = yaml.safe_load(stream)
-            for component in manifest["status"]["components"]:
-                for asset in component["assets"]:
-                    if asset["name"] == "pause-image":
-                        with open(f"{sys.argv[2]}/PAUSE_IMAGE", "w+") as pause_image:
-                            pause_image.write(asset["image"]["uri"])
-                    elif asset["name"] == "coredns-image":
-                        with open(f"{sys.argv[2]}/COREDNS_IMAGE", "w+") as coredns_image:
-                            coredns_image.write(asset["image"]["uri"])
-                    elif asset["name"] == "metrics-server-image":
-                        with open(f"{sys.argv[2]}/METRICS_SERVER_IMAGE", "w+") as metrics_server_image:
-                            metrics_server_image.write(asset["image"]["uri"])
+            with open(f"{sys.argv[2]}/images-list", "w+") as list:
+                for component in manifest["status"]["components"]:
+                    for asset in component["assets"]:
+                        if asset["name"] == "pause-image":
+                            list.write("PAUSE_IMAGE={}\n".format(asset["image"]["uri"]))
+                        elif asset["name"] == "coredns-image":
+                            list.write("COREDNS_IMAGE={}\n".format(asset["image"]["uri"]))
+                        elif asset["name"] == "metrics-server-image":
+                            list.write("METRICS_SERVER_IMAGE={}\n".format(asset["image"]["uri"]))
         except yaml.YAMLError as exc:
             print(exc)
