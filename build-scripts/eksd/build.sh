c2256c208bc5858be2485c5568ec4b652c9cd13a
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2022-09-12 16:12:38 +0300

    Update the vesion to vX.Y-Z

--- build-scripts/eksd/build.sh
+++ build-scripts/eksd/build.sh
@@ -11,15 +11,12 @@ else
   (cd "${SNAPCRAFT_PART_BUILD}/eks-distro" && git fetch --all && git pull)
 fi
 
-EKS_TRACK=$(echo "${KUBE_TRACK}" | cut -f1,2 -d'.' | tr -s . -)
 EKS_RELEASE=$(cat "${SNAPCRAFT_PART_BUILD}"/eks-distro/release/"${EKS_TRACK}"/production/RELEASE)
 
-snapcraftctl set-version "$KUBE_TRACK.$EKS_RELEASE"
+# The version we want to set to the package should look something like v1.23-5
+snapcraftctl set-version "v$KUBE_TRACK-$EKS_RELEASE"
 
 curl "https://distro.eks.amazonaws.com/kubernetes-$EKS_TRACK/kubernetes-$EKS_TRACK-eks-$EKS_RELEASE.yaml" -o "${SNAPCRAFT_PART_BUILD}/kubernetes-$EKS_TRACK-eks-$EKS_RELEASE.yaml"
 
 mkdir -p $INSTALL/images
 python3 parse-manifest.py "${SNAPCRAFT_PART_BUILD}/kubernetes-$EKS_TRACK-eks-$EKS_RELEASE.yaml" "$INSTALL/images"
-
-export KUBE_VERSION=$(cat "$SNAPCRAFT_PART_BUILD/eksd-components/kubernetes/git-tag")
-cat "$SNAPCRAFT_PART_BUILD/eksd-components/kubernetes/git-tag" > $SNAPCRAFT_STAGE/KUBE_VERSION
