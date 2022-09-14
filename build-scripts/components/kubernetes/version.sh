cddc20139cd2b54ec92f9a63524056b9101ff671
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2022-09-14 08:43:08 +0300

    Update versioning and tests

--- build-scripts/components/kubernetes/version.sh
+++ build-scripts/components/kubernetes/version.sh
@@ -2,14 +2,11 @@
 
 KUBE_TRACK="${KUBE_TRACK:-1.23}"            # example: "1.24"
 KUBE_VERSION="${KUBE_VERSION:-}"        # example: "v1.24.2"
+EKS_TRACK=$(echo "${KUBE_TRACK}" | cut -f1,2 -d'.' | tr -s . -) # example: "1-24"
+
 
 if [ -z "${KUBE_VERSION}" ]; then
-  if [ -z "${KUBE_TRACK}" ]; then
-    KUBE_VERSION="$(curl -L --silent "https://dl.k8s.io/release/stable.txt")"
-  else
-    KUBE_TRACK="${KUBE_TRACK#v}"
-    KUBE_VERSION="$(curl -L --silent "https://dl.k8s.io/release/stable-${KUBE_TRACK}.txt")"
-  fi
+  KUBE_VERSION=$(curl -L --silent https://raw.githubusercontent.com/aws/eks-distro/main/projects/kubernetes/kubernetes/${EKS_TRACK}/GIT_TAG)
 fi
 
 echo "${KUBE_VERSION}"
