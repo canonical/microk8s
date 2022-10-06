08b0692f30be73b002f42f030de3eb8e46808aa8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-06 18:42:45 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/wrappers/dbctl.py
+++ scripts/wrappers/dbctl.py
@@ -107,7 +107,26 @@ def restore(fname_tar, debug=False):
     kine_ep = get_kine_endpoint()
     with tempfile.TemporaryDirectory() as tmpdirname:
         with tarfile.open(fname_tar, "r:gz") as tar:
-            tar.extractall(path=tmpdirname)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=tmpdirname)
         if fname_tar.endswith(".tar.gz"):
             fname = fname_tar[:-7]
         else:
