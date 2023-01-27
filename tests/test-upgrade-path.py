67d7cfce95d4ee04bb85c77b110e9ef76c129ee5
Sachin Kumar Singh <sachin.singh@canonical.com>
2023-01-27 14:28:18 +0530

    Make sure for 1.25 branch we use the 1.25 upgrade path

--- tests/test-upgrade-path.py
+++ tests/test-upgrade-path.py
@@ -58,7 +58,7 @@ class TestUpgradePath(object):
 
         # channel = "1.{}/stable".format(start_channel)
 
-        channel = "latest/edge/strict"
+        channel = "1.25-strict/edge"
         print("Installing {}".format(channel))
         cmd = "sudo snap install microk8s --channel={}".format(channel)
         run_until_success(cmd)
