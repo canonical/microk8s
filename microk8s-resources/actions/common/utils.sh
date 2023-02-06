83bd2bb63cd9efb089054e88dab8ee7b4a2bb181
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2023-02-06 10:33:58 +0200

    Lowercase hostname for kubernetes RBAC

--- microk8s-resources/actions/common/utils.sh
+++ microk8s-resources/actions/common/utils.sh
@@ -706,7 +706,7 @@ function update_configs {
   $SNAP/bin/sed -i 's/PASSWORD/'"${admin_token}"'/g' ${SNAP_DATA}/credentials/client.config
   # Create the known tokens
   proxy_token=`grep kube-proxy ${SNAP_DATA}/credentials/known_tokens.csv | cut -d, -f1`
-  hostname=$(hostname)
+  hostname=$(hostname | tr '[:upper:]' '[:lower:]')
   kubelet_token=`grep kubelet-0, ${SNAP_DATA}/credentials/known_tokens.csv | cut -d, -f1`
   controller_token=`grep kube-controller-manager ${SNAP_DATA}/credentials/known_tokens.csv | cut -d, -f1`
   scheduler_token=`grep kube-scheduler ${SNAP_DATA}/credentials/known_tokens.csv | cut -d, -f1`
