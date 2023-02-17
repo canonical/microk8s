f3747cc501b40ba73f80f1f57391a78fb56b20d2
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2023-02-17 10:21:01 +0200

    Cluster agent built for FIPS

--- build-scripts/components/cluster-agent/build.sh
+++ build-scripts/components/cluster-agent/build.sh
@@ -4,5 +4,6 @@ export INSTALL="${1}/bin"
 export GOEXPERIMENT=opensslcrypto
 mkdir -p "${INSTALL}"
 
-make cluster-agent
+go mod tidy
+CGO_ENABLED=1 go build -o cluster-agent ./main.go
 cp cluster-agent "${INSTALL}"
