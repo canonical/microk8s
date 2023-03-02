c41d4b852ce1250339c5cd175a0c23bd3c54f484
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2023-03-02 20:15:24 +0200

    Use a containerd plugin for sideloading images (#3805)

--- build-scripts/components/containerd/build.sh
+++ build-scripts/components/containerd/build.sh
@@ -3,6 +3,12 @@
 INSTALL="${1}/bin"
 mkdir -p "${INSTALL}"
 
+VERSION="${2}"
+REVISION=$(git rev-parse HEAD)
+
+sed -i "s,^VERSION.*$,VERSION=${VERSION}," Makefile
+sed -i "s,^REVISION.*$,REVISION=${REVISION}," Makefile
+
 for bin in ctr containerd containerd-shim containerd-shim-runc-v1 containerd-shim-runc-v2; do
   make "bin/${bin}"
   cp "bin/${bin}" "${INSTALL}/${bin}"
