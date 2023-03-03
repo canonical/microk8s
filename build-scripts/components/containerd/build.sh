bf24da7e76ba117ed3e719a225adb69f10f5f812
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2023-03-03 14:33:49 +0200

    Containerd and runc build with CGO (#3811)

--- build-scripts/components/containerd/build.sh
+++ build-scripts/components/containerd/build.sh
@@ -4,6 +4,7 @@ INSTALL="${1}/bin"
 mkdir -p "${INSTALL}"
 
 for bin in ctr containerd containerd-shim containerd-shim-runc-v1 containerd-shim-runc-v2; do
+  export SHIM_CGO_ENABLED=1
   make "bin/${bin}"
   cp "bin/${bin}" "${INSTALL}/${bin}"
 done
