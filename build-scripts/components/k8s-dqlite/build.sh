18c3cd30da9429d97d5a15ed8c4edd2a14bafd03
Cole Miller <cole.miller@canonical.com>
2023-03-08 00:34:29 -0500

    Include debuginfo for dqlite-related Go code
    Signed-off-by: Cole Miller <cole.miller@canonical.com>

--- build-scripts/components/k8s-dqlite/build.sh
+++ build-scripts/components/k8s-dqlite/build.sh
@@ -6,6 +6,6 @@ mkdir -p "${INSTALL}"
 export CGO_LDFLAGS_ALLOW="-Wl,-z,now"
 export CGO_CFLAGS="-I${SNAPCRAFT_STAGE}/usr/include/"
 export CGO_LDFLAGS="-L${SNAPCRAFT_STAGE}/lib"
-go build -ldflags "-s -w" -tags libsqlite3,dqlite .
+go build -tags libsqlite3,dqlite .
 
 cp k8s-dqlite "${INSTALL}/k8s-dqlite"
