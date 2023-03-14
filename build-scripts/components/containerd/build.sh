f3a9d5e7f592e4b0e08f03293ad58265d9c208fb
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2023-03-14 16:46:06 +0200

    Static link containerd and runc (#3839)

--- build-scripts/components/containerd/build.sh
+++ build-scripts/components/containerd/build.sh
@@ -3,6 +3,13 @@
 INSTALL="${1}/bin"
 mkdir -p "${INSTALL}"
 
+VERSION="${2}"
+REVISION=$(git rev-parse HEAD)
+
+sed -i "s,^VERSION.*$,VERSION=${VERSION}," Makefile
+sed -i "s,^REVISION.*$,REVISION=${REVISION}," Makefile
+
+export STATIC=1
 for bin in ctr containerd containerd-shim containerd-shim-runc-v1 containerd-shim-runc-v2; do
   make "bin/${bin}"
   cp "bin/${bin}" "${INSTALL}/${bin}"
