88d4f60edcb2605bb5f57e2b7923283016effb99
Sachin Kumar Singh <sachin.singh@canonical.com>
2023-03-21 12:54:03 +0530

    Increase time to detect nodes

--- tests/test-cluster.py
+++ tests/test-cluster.py
@@ -375,6 +375,7 @@ class TestCluster(object):
 
         # Wait for nodes to be ready
         print("Waiting for node to register")
+        time.sleep(10)
         connected_nodes = vm_master.run("/snap/bin/microk8s.kubectl get no")
         while "NotReady" in connected_nodes.decode() and vm.vm_name in connected_nodes.decode():
             time.sleep(5)
