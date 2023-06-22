357a1e7f010a86c952ba055376ee01c107375720
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2023-06-22 16:16:53 +0300

    Update to multipass 1.12.0

--- installer/vm_providers/_multipass/_windows.py
+++ installer/vm_providers/_multipass/_windows.py
@@ -39,12 +39,12 @@ logger = logging.getLogger(__name__)
 
 
 _MULTIPASS_RELEASES_API_URL = "https://api.github.com/repos/canonical/multipass/releases"
-_MULTIPASS_DL_VERSION = "1.11.1"
+_MULTIPASS_DL_VERSION = "1.12.0"
 _MULTIPASS_DL_NAME = "multipass-{version}+win-win64.exe".format(version=_MULTIPASS_DL_VERSION)
 
 # Download multipass installer and calculate hash:
 #   python3 -c "from installer.common.file_utils import calculate_sha3_384; print(calculate_sha3_384('$HOME/Downloads/multipass-1.11.1+win-win64.exe'))"  # noqa: E501
-_MULTIPASS_DL_SHA3_384 = "7691383eb0f4def0f9e2b5c77f04424756a63f222b3500bdc8fb25bf4725f1c0ce3bd0cb0b7cff7f79d8f489e199225b"  # noqa: E501
+_MULTIPASS_DL_SHA3_384 = "ddba66059052a67fa6a363729b75aca374591bc5a2531c938dd70d63f683c22108d5c2ab77025b818b31f69103228eee"  # noqa: E501
 
 
 def windows_reload_multipass_path_env():
