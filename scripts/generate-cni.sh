ed0ad39222f9145d6e2c04ab33f0830ad53a6937
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2023-07-07 16:01:47 +0300

    update the cni generation script
    - add a configuration option for which cni to deploy (only calico)
    - group calico-specific and service-specific configuration
    - add validation for the specified configuration options
    - add CALICO_VETH_MTU config option

--- scripts/generate-cni.sh
+++ scripts/generate-cni.sh
@@ -7,47 +7,78 @@ if [ -e "${SNAP_DATA}/args/cni-env" ]; then
     source "${SNAP_DATA}/args/cni-env"
 fi
 
-CNI="${SNAP_DATA}/args/cni-network/cni.yaml"
-RESOURCES="${SNAP}/upgrade-scripts/000-switch-to-calico/resources"
+CNI_YAML="${SNAP_DATA}/args/cni-network/cni.yaml"
+CALICO_RESOURCES="${SNAP}/upgrade-scripts/000-switch-to-calico/resources"
+
 CLUSTER_CIDR=""
 SERVICE_CIDR=""
 
 
-function handle_ipv4 {
-  # Update the cni.yaml with the IPv4 setup and update the cluster and service variables 
-  sed -i '/"type": "calico-ipam"/i \              "assign_ipv4": "true",' "$CNI"
-  if ! [[ -z "${IPv4_CLUSTER_CIDR}" ]] ; then
-    sed -i 's%10.1.0.0/16%'"${IPv4_CLUSTER_CIDR}"'%g' "$CNI"
-    CLUSTER_CIDR="${IPv4_CLUSTER_CIDR}"
+function handle_calico {
+  # Start with the default CNI and patch it based on the user needs
+  cp "${CALICO_RESOURCES}/calico.yaml" "$CNI_YAML"
+
+  # IPv4 configuration
+  if test "x${IPv4_SUPPORT}" = "xtrue"; then
+    # Update the calico cni.yaml with IPv4 specific configurations
+    sed -i '/"type": "calico-ipam"/i \              "assign_ipv4": "true",' "$CNI_YAML"
+    sed -i 's%10.1.0.0/16%'"${IPv4_CLUSTER_CIDR}"'%g' "$CNI_YAML"
   fi
-  if ! [[ -z "${IPv4_SERVICE_CIDR}" ]] ; then
-    SERVICE_CIDR="${IPv4_SERVICE_CIDR}"
+
+  # IPv6 configuration
+  if test "x${IPv6_SUPPORT}" = "xtrue"; then
+    sed -i '/"type": "calico-ipam"/i \              "assign_ipv6": "true",' "$CNI_YAML"
+    sed -i '/FELIX_IPV6SUPPORT/{n;s/.*/\              value: "true"/}' "$CNI_YAML"
+    sed -i '/CALICO_IPV6POOL_VXLAN/{n;s/.*/\              value: "Always"/}' "$CNI_YAML"
+    sed -i '/Enable or Disable VXLAN on the default IPv6 IP pool./a \            - name: IP6' "$CNI_YAML"
+    sed -i '/- name: IP6/a \              value: "autodetect"' "$CNI_YAML"
+
+    sed -i '/Enable or Disable VXLAN on the default IPv6 IP pool./a \            - name: CALICO_IPV6POOL_CIDR' "$CNI_YAML"
+    sed -i '/CALICO_IPV6POOL_CIDR/a \              value: '"${IPv6_CLUSTER_CIDR}" "$CNI_YAML"
+  fi
+
+  # Other configuration
+  if [ ! -z "${CALICO_VETH_MTU}" ]; then
+    sed -i 's,veth_mtu: "0",veth_mtu: "'"${CALICO_VETH_MTU}"'",' "$CNI_YAML"
   fi
 }
 
-function handle_ipv6 {
-  # Update the cni.yaml with the IPv6 setup and update the cluster and service variables 
 
-  sed -i '/"type": "calico-ipam"/i \              "assign_ipv6": "true",' "$CNI"
-  sed -i '/FELIX_IPV6SUPPORT/{n;s/.*/\              value: "true"/}' "$CNI"
-  sed -i '/CALICO_IPV6POOL_VXLAN/{n;s/.*/\              value: "Always"/}' "$CNI"
-  sed -i '/Enable or Disable VXLAN on the default IPv6 IP pool./a \            - name: IP6' "$CNI"
-  sed -i '/- name: IP6/a \              value: "autodetect"' "$CNI"
-  if ! [[ -z "${IPv6_CLUSTER_CIDR}" ]] ; then
-    sed -i '/Enable or Disable VXLAN on the default IPv6 IP pool./a \            - name: CALICO_IPV6POOL_CIDR' "$CNI"
-    sed -i '/CALICO_IPV6POOL_CIDR/a \              value: '"${IPv6_CLUSTER_CIDR}" "$CNI"
-    if [[ -z "${CLUSTER_CIDR}" ]]; then
-      CLUSTER_CIDR="${IPv6_CLUSTER_CIDR}"
-    else
-      CLUSTER_CIDR="${CLUSTER_CIDR}","${IPv6_CLUSTER_CIDR}"
+function validate_configuration {
+  if test "x${IPv4_SUPPORT}" = "xtrue"; then
+    if test "x${IPv4_CLUSTER_CIDR}" = "x"; then
+      echo "Failed: IPv4_SUPPORT is true, but no IPv4_CLUSTER_CIDR was set"
+      exit 1
+    fi
+    if test "x${IPv4_SERVICE_CIDR}" = "x"; then
+      echo "Failed: IPv4_SUPPORT is true, but no IPv4_SERVICE_CIDR was set"
+      exit 1
     fi
   fi
-  if ! [[ -z "${IPv6_SERVICE_CIDR}" ]] ; then
-    if [[ -z "${SERVICE_CIDR}" ]]; then
-      SERVICE_CIDR="${IPv6_SERVICE_CIDR}"
-    else
-      SERVICE_CIDR="${SERVICE_CIDR}","${IPv6_SERVICE_CIDR}"
+
+  if test "x${IPv6_SUPPORT}" = "xtrue"; then
+    if test "x${IPv6_CLUSTER_CIDR}" = "x"; then
+      echo "Failed: IPv6_SUPPORT is true, but no IPv6_CLUSTER_CIDR was set"
+      exit 1
     fi
+    if test "x${IPv6_SERVICE_CIDR}" = "x"; then
+      echo "Failed: IPv6_SUPPORT is true, but no IPv6_SERVICE_CIDR was set"
+      exit 1
+    fi
+  fi
+
+  if test x"${IPv4_SUPPORT}" = "xtrue" && test x"${IPv6_SUPPORT}" = "xtrue"; then
+    CLUSTER_CIDR="${IPv4_CLUSTER_CIDR},${IPv6_CLUSTER_CIDR}"
+    SERVICE_CIDR="${IPv4_SERVICE_CIDR},${IPv6_SERVICE_CIDR}"
+  elif test x"${IPv4_SUPPORT}" = "xtrue"; then
+    CLUSTER_CIDR="${IPv4_CLUSTER_CIDR}"
+    SERVICE_CIDR="${IPv4_SERVICE_CIDR}"
+  elif test x"${IPv6_SUPPORT}" = "xtrue"; then
+    CLUSTER_CIDR="${IPv6_CLUSTER_CIDR}"
+    SERVICE_CIDR="${IPv6_SERVICE_CIDR}"
+  else
+    echo "Failed: At least one of IPv4_SUPPORT or IPv6_SUPPORT must be true"
+    exit 1
   fi
 }
 
@@ -62,13 +93,17 @@ function setup_service_arguments {
   refresh_opt_in_local_config cluster-cidr "${CLUSTER_CIDR}" kube-proxy
 }
 
-
-# Start with the default CNI and patch it based on the user needs
-cp "${RESOURCES}/calico.yaml" "$CNI"
-if ! [[ -z "${IPv4_SUPPORT}" ]] && [ "${IPv4_SUPPORT}" == "true" ]; then
-  handle_ipv4
-fi
-if ! [[ -z "${IPv6_SUPPORT}" ]] && [ "${IPv6_SUPPORT}" == "true" ]; then
-  handle_ipv6
-fi
+# Common setup, validation and setup service arguments
+validate_configuration
 setup_service_arguments
+
+# Do setup of the chosen CNI
+case "${CNI}" in
+  calico)
+    handle_calico
+    ;;
+  *)
+    echo "CNI must be set to 'calico'"
+    exit 1
+    ;;
+esac
