e03bea3c2c26062cd2fb2156627f8a657dd87b36
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2023-07-11 17:11:20 +0300

    Do not warn for cgroups if cgroups v2 is available

--- scripts/inspect.sh
+++ scripts/inspect.sh
@@ -271,9 +271,11 @@ function suggest_fixes {
 
   if ! grep -q 'cgroup/memory' /proc/self/mountinfo; then
     if ! grep -q 'cgroup2' /proc/self/mountinfo; then
-      printf -- '\033[0;33mWARNING: \033[0m The memory cgroup is not enabled. \n'
-      printf -- 'The cluster may not be functioning properly. Please ensure cgroups are enabled \n'
-      printf -- 'See for example: https://microk8s.io/docs/install-alternatives#heading--arm \n'
+      if ! mount -t cgroup2 | grep -q 'type cgroup2'; then
+        printf -- '\033[0;33mWARNING: \033[0m The memory cgroup is not enabled. \n'
+        printf -- 'The cluster may not be functioning properly. Please ensure cgroups are enabled \n'
+        printf -- 'See for example: https://microk8s.io/docs/install-alternatives#heading--arm \n'
+      fi
     fi
   fi
 
