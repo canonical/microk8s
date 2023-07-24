58ecb25e27f0bb611a64f2e53166598d8d2e13d0
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2023-07-24 17:12:54 +0300

    Start in FIPS machines and build fixes on runc and containerd

--- build-scripts/components/runc/build.sh
+++ build-scripts/components/runc/build.sh
@@ -4,5 +4,6 @@ export INSTALL="${1}/bin"
 mkdir -p "${INSTALL}"
 
 export CGO_ENABLED=1
+export GOEXPERIMENT=opensslcrypto
 make BUILDTAGS="seccomp apparmor" EXTRA_LDFLAGS="-s -w"
 cp runc "${INSTALL}/runc"
