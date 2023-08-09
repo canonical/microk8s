e7b6280bd4e808a08603f26cd0f8500e4f50e7da
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2023-08-09 16:02:30 +0300

    Use the same params when building cluster-agent

--- build-scripts/components/cluster-agent/build.sh
+++ build-scripts/components/cluster-agent/build.sh
@@ -4,5 +4,5 @@ export INSTALL="${1}/bin"
 export GOEXPERIMENT=opensslcrypto
 mkdir -p "${INSTALL}"
 
-CGO_ENABLED=1 go build -o cluster-agent ./main.go
+CGO_ENABLED=1 go build -ldflags '-s -w' -o cluster-agent ./main.go
 cp cluster-agent "${INSTALL}"
