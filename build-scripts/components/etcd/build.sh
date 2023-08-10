0dc976f59c6fd103567398175fcf3738d10a4aa2
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2023-08-10 14:57:14 +0300

    Revert "Fixes and follow-up FIPS-ready patches (#4135)"
    This reverts commit c1d1fc200e94ddc51d20372cccca1c149dbafc1b.

--- build-scripts/components/etcd/build.sh
+++ build-scripts/components/etcd/build.sh
@@ -3,8 +3,7 @@
 export INSTALL="${1}"
 mkdir -p "${INSTALL}"
 
-sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/' build.sh
-GOEXPERIMENT=opensslcrypto GO_LDFLAGS="-s -w" GO_BUILD_FLAGS="-v" ./build.sh
+GO_LDFLAGS="-s -w" GO_BUILD_FLAGS="-v" ./build.sh
 
 for bin in etcd etcdctl; do
   cp "bin/${bin}" "${INSTALL}/${bin}"
