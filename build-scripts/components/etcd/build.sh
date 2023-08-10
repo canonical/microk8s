c1d1fc200e94ddc51d20372cccca1c149dbafc1b
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2023-08-10 10:05:23 +0300

    Fixes and follow-up FIPS-ready patches (#4135)

--- build-scripts/components/etcd/build.sh
+++ build-scripts/components/etcd/build.sh
@@ -3,7 +3,8 @@
 export INSTALL="${1}"
 mkdir -p "${INSTALL}"
 
-GO_LDFLAGS="-s -w" GO_BUILD_FLAGS="-v" ./build.sh
+sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/' build.sh
+GOEXPERIMENT=opensslcrypto GO_LDFLAGS="-s -w" GO_BUILD_FLAGS="-v" ./build.sh
 
 for bin in etcd etcdctl; do
   cp "bin/${bin}" "${INSTALL}/${bin}"
