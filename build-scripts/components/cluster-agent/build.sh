9af69f00dcc213c8e527d7d4292ca6649501e4a0
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2023-08-10 14:58:27 +0300

    Revert "FIPS-ready patches (#4096)"
    This reverts commit d941ef90b78d3751ac2847ac0da336244ed9aaba.

--- build-scripts/components/cluster-agent/build.sh
+++ build-scripts/components/cluster-agent/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 export INSTALL="${1}/bin"
-export GOEXPERIMENT=opensslcrypto
 mkdir -p "${INSTALL}"
 
-CGO_ENABLED=1 go build -ldflags '-s -w' -o cluster-agent ./main.go
+make cluster-agent
 cp cluster-agent "${INSTALL}"
