48bfd3f8b43402b8b1faa1d023fdff08f01303e8
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2023-08-21 18:27:38 +0300

    Correct args

--- scripts/wrappers/join.py
+++ scripts/wrappers/join.py
@@ -827,13 +827,13 @@ def join_dqlite_master_node(info, master_ip):
         set_arg("--etcd-servers", info["etcd_servers"], "kube-apiserver")
         if info.get("etcd_ca"):
             store_cert("remote-etcd-ca.crt", info["etcd_ca"])
-            set_arg("etcd-cafile", "${SNAP_DATA}/certs/remote-etcd-ca.crt", "kube-apiserver")
+            set_arg("--etcd-cafile", "${SNAP_DATA}/certs/remote-etcd-ca.crt", "kube-apiserver")
         if info.get("etcd_cert"):
             store_cert("remote-etcd.crt", info["etcd_cert"])
-            set_arg("etcd-certfile", "${SNAP_DATA}/certs/remote-etcd.crt", "kube-apiserver")
+            set_arg("--etcd-certfile", "${SNAP_DATA}/certs/remote-etcd.crt", "kube-apiserver")
         if info.get("etcd_key"):
             store_cert("remote-etcd.key", info["etcd_key"])
-            set_arg("etcd-keyfile", "${SNAP_DATA}/certs/remote-etcd.crt", "kube-apiserver")
+            set_arg("--etcd-keyfile", "${SNAP_DATA}/certs/remote-etcd.crt", "kube-apiserver")
 
         mark_no_dqlite()
         service("restart", "k8s-dqlite")
