8a28e8e0a72de433a9cfd57c3420e72d989b99f2
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2023-09-01 09:13:16 +0300

    Use statically-linked k8s-dqlite (#4178)

--- build-scripts/components/k8s-dqlite/build.sh
+++ build-scripts/components/k8s-dqlite/build.sh
@@ -3,10 +3,7 @@
 INSTALL="${1}/bin"
 mkdir -p "${INSTALL}"
 
-export CGO_LDFLAGS_ALLOW="-Wl,-z,now"
-export CGO_CFLAGS="-I${SNAPCRAFT_STAGE}/usr/include/"
-export CGO_LDFLAGS="-L${SNAPCRAFT_STAGE}/lib"
+make static -j
 
-go build -ldflags "-s -w" -tags libsqlite3,dqlite .
-
-cp k8s-dqlite "${INSTALL}/k8s-dqlite"
+cp bin/static/dqlite "${INSTALL}/dqlite"
+cp bin/static/k8s-dqlite "${INSTALL}/k8s-dqlite"
