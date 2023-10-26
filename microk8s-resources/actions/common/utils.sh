715e7e075889f7014c480aff8014c3d38693b905
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2023-10-26 20:14:44 +0300

    fix typo: sanatise -> sanitize (#4249)
    * fix typo: sanatise -> sanitize
    
    * use consistent spelling
    
    * prefer british english
    
    long live the queen

--- microk8s-resources/actions/common/utils.sh
+++ microk8s-resources/actions/common/utils.sh
@@ -248,8 +248,8 @@ remove_args() {
 }
 
 
-sanatise_argskubeapi_server() {
-  # Function to sanitize arguments for API server
+sanitise_args_kubeapi_server() {
+  # Function to sanitise arguments for API server
   local args=(
     # Removed klog flags from 1.26+
     # https://github.com/kubernetes/enhancements/blob/master/keps/sig-instrumentation/2845-deprecate-klog-specific-flags-in-k8s-components/README.md
@@ -283,8 +283,8 @@ sanatise_argskubeapi_server() {
 }
 
 
-sanatise_argskubelet() {
-  # Function to sanitize arguments for kubelet
+sanitise_args_kubelet() {
+  # Function to sanitise arguments for kubelet
   local args=(
     # Removed klog flags from 1.26+
     # https://github.com/kubernetes/enhancements/blob/master/keps/sig-instrumentation/2845-deprecate-klog-specific-flags-in-k8s-components/README.md
@@ -325,8 +325,8 @@ sanatise_argskubelet() {
 }
 
 
-sanatise_argskube_proxy() {
-  # Function to sanitize arguments for kube-proxy
+sanitise_args_kube_proxy() {
+  # Function to sanitise arguments for kube-proxy
 
   # userspace proxy-mode is not allowed on the 1.26+ k8s
   # https://kubernetes.io/blog/2022/11/18/upcoming-changes-in-kubernetes-1-26/#removal-of-kube-proxy-userspace-modes
@@ -357,8 +357,8 @@ sanatise_argskube_proxy() {
 }
 
 
-sanatise_argskube_controller_manager() {
-  # Function to sanitize arguments for kube-controller-manager
+sanitise_args_kube_controller_manager() {
+  # Function to sanitise arguments for kube-controller-manager
   local args=(
     # Removed klog flags from 1.26+
     # https://github.com/kubernetes/enhancements/blob/master/keps/sig-instrumentation/2845-deprecate-klog-specific-flags-in-k8s-components/README.md
@@ -386,8 +386,8 @@ sanatise_argskube_controller_manager() {
 }
 
 
-sanatise_argskube_scheduler() {
-  # Function to sanitize arguments for kube-scheduler
+sanitise_args_kube_scheduler() {
+  # Function to sanitise arguments for kube-scheduler
   local args=(
     # Removed klog flags from 1.26+
     # https://github.com/kubernetes/enhancements/blob/master/keps/sig-instrumentation/2845-deprecate-klog-specific-flags-in-k8s-components/README.md
