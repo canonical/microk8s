c3341e1f1f99c0051372a50e7926d3819b880a01
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-02-20 15:11:18 +0200

    do not produce static PIE runc executables on arm64 (#4427) (#4432)

--- build-scripts/components/runc/build.sh
+++ build-scripts/components/runc/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+VERSION="${2}"
+
 export INSTALL="${1}/bin"
 mkdir -p "${INSTALL}"
+
+export COMMIT="$(git describe --always --long "${VERSION}")"
 
 make BUILDTAGS="seccomp apparmor" EXTRA_LDFLAGS="-s -w" static
 cp runc "${INSTALL}/runc"
