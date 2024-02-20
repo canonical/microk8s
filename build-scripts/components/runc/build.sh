5653717d4d4f8313d9b9f42a7429529a3eebf34b
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-02-20 13:15:54 +0200

    add note for runc COMMIT

--- build-scripts/components/runc/build.sh
+++ build-scripts/components/runc/build.sh
@@ -5,6 +5,7 @@ VERSION="${2}"
 export INSTALL="${1}/bin"
 mkdir -p "${INSTALL}"
 
+# Ensure `runc --version` prints the correct release commit
 export COMMIT="$(git describe --always --long "${VERSION}")"
 
 make BUILDTAGS="seccomp apparmor" EXTRA_LDFLAGS="-s -w" static
