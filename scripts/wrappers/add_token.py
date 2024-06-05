507b153eabb940d0025bb951b5cc993c3a2fd969
Shrishti Karkera <shrishtikarkera@sanji.lan>
2024-06-05 09:53:11 -0400

    changed print_short in add_token.py

--- scripts/wrappers/add_token.py
+++ scripts/wrappers/add_token.py
@@ -120,10 +120,15 @@ def print_yaml(token, check):
 
 def print_short(token, check):
     default_ip, all_ips, port = get_network_info()
-
+    default_addr = default_ip+":"+port+"/"+token+"/"+check
+    addr_set = set()
+    addr_set.add(default_addr)
     print(f"microk8s join {default_ip}:{port}/{token}/{check}")
     for ip in all_ips:
-        print(f"microk8s join {ip}:{port}/{token}/{check}")
+        addr = ip+":"+port+"/"+token+"/"+check
+        if addr not in addr_set:
+            print(f"microk8s join {ip}:{port}/{token}/{check}")
+            addr_set.add(addr)
 
 
 if __name__ == "__main__":
