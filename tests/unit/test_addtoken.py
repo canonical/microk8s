ca71de0501a3065a8c6391a817b18d9a1b681afe
Shrishti Karkera <shrishtikarkera@sanji.lan>
2024-06-07 11:31:06 -0400

    changed and tested add_token.py #4526

--- tests/unit/test_addtoken.py
+++ tests/unit/test_addtoken.py
@@ -1,9 +1,10 @@
-"""from unittest import mock, TestCase
+from unittest import mock, TestCase
 from add_token import print_short
 
 class test_at(TestCase):
-
-    @mock.patch('add_token.get_network_info', return_value=['10.23.53.54', ['10.23.53.54', '34,54.34.54'], str(32)])
-    def test_add_token(self, addtoken):
-        self.assertEqual(print_short('t', 'c'), 2)
-"""
+    @mock.patch('builtins.print')
+    @mock.patch('add_token.get_network_info', return_value=['10.23.53.54', ['10.23.53.54'], str(32)])
+    def test_add_token(self, addtoken, mock_print):
+        # self.assertEqual(print_short('t', 'c'), 2)
+        print_short('t', 'c')
+        mock_print.assert_called_with('microk8s join 10.23.53.54:32/t/c')
