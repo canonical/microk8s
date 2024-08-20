55f20f055643ec94afa110c499f1eaf798582dd1
Berkay Tekin Oz <berkay.tekinoz@canonical.com>
2024-08-20 18:07:35 +0000

    Update k8s-dqlite version to use updated go dqlite

--- build-scripts/components/k8s-dqlite/build.sh
+++ build-scripts/components/k8s-dqlite/build.sh
@@ -3,9 +3,9 @@
 INSTALL="${1}/bin"
 mkdir -p "${INSTALL}"
 
-export CGO_LDFLAGS_ALLOW="-Wl,-z,now"
 export CGO_CFLAGS="-I${SNAPCRAFT_STAGE}/usr/include/"
 export CGO_LDFLAGS="-L${SNAPCRAFT_STAGE}/lib"
+export LD_LIBRARY_PATH="${SNAPCRAFT_STAGE}/lib"
 
 go build -ldflags "-s -w" -tags libsqlite3,dqlite .
 
--- build-scripts/components/k8s-dqlite/version.sh
+++ build-scripts/components/k8s-dqlite/version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "v1.1.7"
+echo "KU-1404/go-dqlite-update-test"
