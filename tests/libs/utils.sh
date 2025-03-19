1a3d13f3274b95816bf182b0a5a92c74fb1e7639
louiseschmidtgen <louise.schmidtgen@canonical.com>
2025-03-19 14:30:40 +0100

    bump timeouts

--- tests/libs/utils.sh
+++ tests/libs/utils.sh
@@ -13,15 +13,14 @@ function create_machine() {
   lxc launch -p default -p microk8s "$DISTRO" "$NAME"
 
   # Allow for the machine to boot and get an IP
-  timeout 60 bash -c 'until lxc list "$NAME" -c4 | grep -q eth; do sleep 5; done' || { echo "Error: $NAME did not get an IP within 60s"; exit 1; }
+  timeout 120 bash -c 'until lxc list "$NAME" -c4 | grep -q eth; do sleep 5; done' || { echo "Error: $NAME did not get an IP within 60s"; exit 1; }
 
   tar cf - ./tests | lxc exec "$NAME" -- tar xvf - -C /root
   DISTRO_DEPS_TMP="${DISTRO//:/_}"
   DISTRO_DEPS="${DISTRO_DEPS_TMP////-}"
   lxc exec "$NAME" -- /bin/bash "/root/tests/lxc/install-deps/$DISTRO_DEPS"
   lxc exec "$NAME" -- reboot
-  timeout 60 bash -c 'until lxc exec "$NAME" -- /bin/true; do sleep 5; done' || { echo "Error: Con
-tainer $NAME did not become ready in 60s"; exit 1; }
+  timeout 600 bash -c 'until lxc exec "$NAME" -- /bin/true; do sleep 5; done' || { echo "Error: Container $NAME did not become ready in 60s"; exit 1; }
 
   trap 'lxc delete '"${NAME}"' --force || true' EXIT
   if [ "$#" -ne 1 ]
@@ -29,8 +28,7 @@ tainer $NAME did not become ready in 60s"; exit 1; }
     lxc exec "$NAME" -- /bin/bash -c "echo HTTPS_PROXY=$PROXY >> /etc/environment"
     lxc exec "$NAME" -- /bin/bash -c "echo https_proxy=$PROXY >> /etc/environment"
     lxc exec "$NAME" -- reboot
-    timeout 60 bash -c 'until lxc exec "$NAME" -- /bin/true; do sleep 5; done' || { echo "Error: Con
-tainer $NAME did not become ready in 60s after applying proxy settings"; exit 1; }
+    timeout 600 bash -c 'until lxc exec "$NAME" -- /bin/true; do sleep 5; done' || { echo "Error: Container $NAME did not become ready in 60s after applying proxy settings"; exit 1; }
   fi
 }
 
